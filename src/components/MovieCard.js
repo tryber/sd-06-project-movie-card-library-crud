91c8b44e55cc7cb5870a1edd313bfa80ba82ca7d
Lais Gurgel <laisgurgelf@gmail.com>
2020-09-23 16:04:39 -0300

    Requisito 3 - MovieCard renderiza informações do filme e link para mais detalhes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    console.log(movie)
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={movie.imagePath} alt={movie.title} />
+        <h1>{movie.title}</h1>
+        <h2>{movie.subtitle}</h2>
+        <p>{movie.storyline}</p>
+        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
       </div>
     );
   }
