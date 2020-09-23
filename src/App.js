c4761a76e0f6698720753108c7aa026093271bdc
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-09-23 16:32:31 -0300

    Resolução do requisito 01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, NewMovie, EditMovie, MovieDetails, NotFound } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -19,6 +19,7 @@ class MovieList extends Component {
 
     return (
       <div data-testid="movie-list">
+        <p>Movie List</p>
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
