67e3fe0e69624e1edce06da2362eead4495d5a8d
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-09-23 15:42:41 -0300

    Acerto CC no primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound, index } from './pages';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
-return (
-  <Router>
-    <Switch>
-      <Route path="/movies/:id/edit" component={EditMovie} /> 
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="/movies/:id" component={MovieDetails} /> 
-      <Route path="/" component={MovieList} />
-    </Switch>
-    <Route path="" component={NotFound} />
-  </Router>
-);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/" component={MovieList} />
+      </Switch>
+      <Route path="" component={NotFound} />
+    </Router>
+  );
 }
 
 export default App;
