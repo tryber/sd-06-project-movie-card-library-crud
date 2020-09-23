5e69245aa2c6f6914d2df6a5188b7315b0abfdce
wfelipefarina <wfelipefarina@gmail.com>
2020-09-23 19:31:17 -0300

    Requisit 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
-import NewMovie from './pages/NewMovie';
-import EditMovie from './pages/EditMovie';
-import NotFound from './pages/NotFound';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
-class App extends Component {
-  render() {
-    return (
-      <main>
-        <BrowserRouter>
-          <div>Movie Card Library CRUD</div>
-          <Switch>
-            <Route path="/movies/:id/edit" component={EditMovie} />
-            <Route path="/movies/new" component={NewMovie} />
-            <Route path="/movies/:id" component={MovieDetails} />
-            <Route path="/" component={MovieList} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </main>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
