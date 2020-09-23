b1dd38eefca30b9546ade00b470ab7221a52bd68
Marcelo Pessini <marceloppessini@gmail.com>
2020-09-23 19:42:01 -0300

    Teste CodeClimate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
+        <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
