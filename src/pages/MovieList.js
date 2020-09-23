85103c33c68f414200dfc8355800e363a4d87460
Dilenio Enderle <dilenio@gmail.com>
2020-09-23 16:41:26 -0300

    2 - Movie list: Ao ser montado, MovieList deve fazer uma requisição para buscar a lista de filmes a ser renderizada

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
+import { MovieCard, Loading } from '../components';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -7,17 +7,36 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.fetchMovies = this.fetchMovies.bind(this);
     this.state = {
       movies: [],
+      loading: true,
     }
   }
 
+  async fetchMovies() {
+    this.setState({
+      loading: true,
+    });
+
+    const movies = await movieAPI.getMovies();
+
+    this.setState({
+      movies,
+      loading: false,
+    });
+  }
+
+  componentDidMount() {
+    this.fetchMovies()
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
-    return (
+    return loading ? <Loading /> : (
       <div data-testid="movie-list">MovieList
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
