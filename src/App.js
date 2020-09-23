796f12376ba66021ba7cd8d9f302a7f97be7614c
Macarena Rodriguez <macarenar697@gmail.com>
2020-09-23 17:13:21 -0300

    1 - Rotas: O componente App deve renderizar BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MovieList, EditMovie, MovieDetails, NewMovie, NotFound } from "./pages";
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+          <Route path="/" component={MovieList}/>
+          <Route path="/movies/new" component={NewMovie}/>
+          <Route path="/movies/:id" component={MovieDetails}/>
+          <Route path="/movies/:id/edit" component={EditMovie}/>
+          <Route path component={NotFound} />
+      </Router>
+    );
+  }
 }
 
 export default App;
