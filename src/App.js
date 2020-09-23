c2530c30f8a3b8fef3aad315c6cc230b9b0e7d5a
Paulo <paulocheloni@gmail.com>
2020-09-23 15:30:16 -0300

    adding first req

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <BrowserRouter>
+      <Switch >
+        <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+        <Route path="/movies/new" render={(props) => <NewMovie {...props} />} />
+        <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route exact path="/" render={(props) => <MovieList {...props} />} />
+        <Route component={NotFound} />
+      </Switch>
+        
+      </BrowserRouter>
+      </div>
   );
 }
 
