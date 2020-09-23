6feab5e6dafa60fc8dce2a183e0a94d669f0d191
Daniel Moura <darthur.moura@gmail.com>
2020-09-23 15:35:31 -0300

    Correcting CC issues (unused import in App)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Loading, MovieCard, MovieForm } from './components';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
