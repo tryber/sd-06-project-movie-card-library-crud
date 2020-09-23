80d9448dbc39a9ddd5b668a4d430ef74dc4bdd01
RenatoCostaDev <renatotrybe@gmail.com>
2020-09-23 16:34:26 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
+import Routes from './components/Routes';
+
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Routes />
     </Router>
   );
 }
