0fe5c5208ea94fba9d75614ae685511f748c4d43
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-23 17:59:37 -0300

    CC, changed single quotes to double quotes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import MovieList from './pages/MovieList';
 import EditMovie from './pages/EditMovie';
 import NewMovie from './pages/NewMovie';
 import MovieDetails from './pages/MovieDetails';
-import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD!</div>
       <Switch>
-        <Route exact path='/movies/:id/edit' component={EditMovie} />
-        <Route exact path='/movies/:id' component={MovieDetails} />
-        <Route exact path='/movies/new' component={NewMovie} />
-        <Route path='/' component={MovieList} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route path="/" component={MovieList} />
       </Switch>
     </BrowserRouter>
   );
