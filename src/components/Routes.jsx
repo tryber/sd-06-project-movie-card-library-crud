79161e2248aa50b0e4ddf075d8ca9f08c95d5fc9
RenatoCostaDev <renatotrybe@gmail.com>
2020-09-23 16:47:11 -0300

    Requisito 1 again

---
+++ src/components/Routes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+import MovieList from '../pages/MovieList';
+import MovieDetails from '../pages/MovieDetails';
+import NewMovie from '../pages/NewMovie';
+import EditMovie from '../pages/EditMovie';
+import NotFound from '../pages/NotFound';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route path='*' component={NotFound} />
+        {/* <Redirect from="*" component={NotFound} /> */}
+      </Switch>
+    );
+  }
+}
+export default Routes;
