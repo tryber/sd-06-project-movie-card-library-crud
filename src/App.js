8d641521d2c6664b30e20d08d1bfee29a4b4676e
CristianoNunes <cristianonunes99@gmail.com>
2020-09-23 18:16:33 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
+
 
 function App() {
   return (
