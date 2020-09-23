fd4a3dc75144f7e84a9472ba183b4f5c578bad82
Alvaro Neto <alvaroshn@gmail.com>
2020-09-23 17:21:32 -0300

    Requisito 4 [1]

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -11,18 +12,19 @@ class MovieDetails extends Component {
     this.state = {
       movies: [],
       isLoading: true,
-    }
+    };
   }
 
   componentDidMount() {
-    movieAPI.getMovie(this.props.match.params.id).then((myFetchMovies) =>
+    const { match } = this.props;
+    movieAPI.getMovie(match.params.id).then((myFetchMovies) =>
     this.setState({
       movies: myFetchMovies,
       isLoading: false,
     }));
   }
   render() {
-    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movies;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = this.state.movies;
     const { isLoading } = this.state;
 
     if (isLoading) return <Loading />;
@@ -36,10 +38,16 @@ class MovieDetails extends Component {
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
         <Link to="/">VOLTAR</Link>
-        <Link to={`/movies/${this.props.match.params.id}/edit`}>EDITAR</Link>   
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieDetails;
