c7eaf0f2b5c870c2478e3694dbc7131a30ecfb80
Alvaro Neto <alvaroshn@gmail.com>
2020-09-23 17:15:11 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movies: [],
+      isLoading: true,
+    }
+  }
+
+  componentDidMount() {
+    movieAPI.getMovie(this.props.match.params.id).then((myFetchMovies) =>
+    this.setState({
+      movies: myFetchMovies,
+      isLoading: false,
+    }));
+  }
+  render() {
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movies;
+    const { isLoading } = this.state;
+
+    if (isLoading) return <Loading />;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={`/movies/${this.props.match.params.id}/edit`}>EDITAR</Link>   
       </div>
     );
   }
