b4d7fd89627c2552a169a72ffef624d711c3567d
Renan Cunha <renan2610@gmail.com>
2020-09-23 16:30:38 -0300

    adicionando requisito 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
