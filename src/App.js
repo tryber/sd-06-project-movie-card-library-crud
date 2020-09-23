d4750be987b83025afd9b76e83f17aeb0233b32c
FlavioSugano <flavio.sugano@gmail.com>
2020-09-23 15:17:51 -0300

    App.js: Criaçao do Switch c/ os Routes para cada pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { EditMovie, MovieDetails, NewMovie, NotFound, MovieList } from './pages';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route path="/404" component={NotFound} />
+      <Route exact path="/" component={MovieList} />
+      <Redirect exact from="*" to="/404" />
+    </Switch>
   );
 }
 
