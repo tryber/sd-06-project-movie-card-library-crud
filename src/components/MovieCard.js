0db23a8e84e9898a3922f8552954f079220fa6dd
Ana Flávia <anaflvia@gmail.com>
2020-09-23 18:36:20 -0300

    modificações em MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,13 +4,11 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { imagePath, title, subtitle, storyline, id } = this.props.movie;
+    const { title, storyline, id } = this.props.movie;
     return (
-      <div data-testid="movie-card">
-        <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
+      <div data-testid="movie-card">        
         <div className="movie-card-body">
           <h4 className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
         <Link to={`/movies/${id}`}>VER DETALHES</Link>
@@ -20,9 +18,7 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  imagePath: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
   storyline: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
 };
