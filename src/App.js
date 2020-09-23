02c55ba21b4de96bfaa0c956b8faca35dcd7433c
Alvaro Neto <alvaroshn@gmail.com>
2020-09-23 19:42:26 -0300

    Requisito 5 [3]

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id" component={MovieDetails} />
         <Route path="/" component={NotFound} />
       </Switch>
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { MovieCard, Loading } from '../components';
+import { Redirect, Route } from 'react-router-dom';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
@@ -7,12 +9,40 @@ class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      shouldRedirect: false,
+      isLoading: true,
+      movie: {},
+    }
   }
 
-  handleSubmit(newMovie) {
+  async handleSubmit(newMovie) {
+    // const myMovie = await movieAPI.createMovie(newMovie);
+    // if (myMovie === 'OK') {
+    //   this.setState({
+    //     shouldRedirect: true,
+    //     isLoading: false,
+    //     movie: newMovie,
+    //   });
+    // }
   }
 
   render() {
+    // const { isLoading, shouldRedirect, movie } = this.state;
+
+    // if (isLoading) return <Loading />;
+
+    // if (shouldRedirect) {
+    //   return (
+    //   <Route>
+    //     <MovieCard movie={movie}/>
+    //     <Redirect to="/" />
+    //   </Route>
+    //   )
+    // }
+ 
+    
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
