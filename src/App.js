3e74c945477748dc48e0b4ef2adc52a7fad15506
Pablo Santos <pablo.trybe@gmail.com>
2020-09-23 18:47:04 -0300

    Fixing CC

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
       <h1>Movie Card Library CRUD</h1>
       <BrowserRouter>
         <Switch>
-          <Route path="/movies/new" render={(props) => <NewMovie {...props} /> }/>
-          <Route exact path="/movies/:id" render={(props) => <MovieDetails {...props} /> }/>
-          <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} /> }/>
-          <Route exact path="/" render={(props) => <MovieList {...props} />}/>
+          <Route path="/movies/new" render={(props) => <NewMovie {...props} />} />
+          <Route exact path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+          <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+          <Route exact path="/" render={(props) => <MovieList {...props} />} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
