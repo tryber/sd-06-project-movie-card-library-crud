5c908f3b51f0260a2c4d1abcb710f5ba71278015
HCaetano <hrcgeo@gmail.com>
2020-09-23 19:08:17 -0300

    Remove commented line from file

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -32,8 +32,6 @@ class MovieList extends Component {
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
         {
