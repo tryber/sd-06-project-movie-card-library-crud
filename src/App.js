f12aae4b6c40e40108aeaac1f51c9f017390dd2e
Micaeli Teixeira <luime6@gmail.com>
2020-09-23 17:20:40 -0300

    Adicionando o componente NewMovie  em App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { MovieList } from './pages';
+import { MovieList, NewMovie } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
       </Switch>
     </BrowserRouter>
   );
