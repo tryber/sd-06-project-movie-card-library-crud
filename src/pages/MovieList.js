c4e3aee68a91bf082de86e8a76dcedf78fce0763
ricardorosa-dev <ricardorosa@gmail.com>
2020-09-23 18:43:01 -0300

    Segundo describe

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,17 +9,43 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: false,
     }
+
+    this.renderMovies = this.renderMovies.bind(this);
+  }
+
+  async fetchMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const moviesJson = await movieAPI.getMovies();
+
+        this.setState({
+          loading: false,
+          movies: moviesJson,
+        });
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  renderMovies() {
+    const { movies } = this.state;
+    return movies.map((movie) => <MovieCard key={movie.title} movie={movie} />);
   }
 
   render() {
     const { movies } = this.state;
+    const loadingElement = <span>Carregando...</span>
 
     // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {this.state.loading ? loadingElement : this.renderMovies()}
       </div>
     );
   }
