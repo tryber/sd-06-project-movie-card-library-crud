9f0326559498aaff194a72a851bbd0218447e1fa
Daniel Moura <darthur.moura@gmail.com>
2020-09-23 15:33:11 -0300

    Req 1: all routes declared in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Loading, MovieCard, MovieForm } from './components';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route component={NotFound} />
     </Router>
   );
 }
