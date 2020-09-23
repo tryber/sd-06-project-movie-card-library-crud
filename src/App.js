7483b274e7b38fdaa5f683610bbc5551aeac1fef
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-09-23 19:25:07 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -8,17 +8,15 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route path="/" component={MovieList} />
-          <Route path="" component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/" component={MovieList} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { imagePath, title, storyline, id } = this.props.movie
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" src={imagePath} />
+        <p>{title}</p>
+        <p>{storyline}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
