a18e519c9a8b49f3d641bcddae3bd5351725c931
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-23 19:39:44 -0300

    Delete movie from movieAPI

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
 
-import * as movieAPI from '../services/movieAPI';
+import { Link } from 'react-router-dom'
 import { Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
+
 
 class MovieDetails extends Component {
   constructor() {
@@ -12,6 +14,8 @@ class MovieDetails extends Component {
       movie: {},
       loading: false,
     };
+
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   componentDidMount() { 
@@ -21,6 +25,11 @@ class MovieDetails extends Component {
         this.setState({ movie: await movieAPI.getMovie(id), loading: false });
       }
     );
+  }
+
+  async handleDelete() {
+    const { id } = this.props.match.params;
+    await movieAPI.deleteMovie(id);
   }
 
   render() {
@@ -42,6 +51,12 @@ class MovieDetails extends Component {
         <p>{`Rating: ${rating}`}</p>
         <div className="actions">
           <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+          <button
+            type="button"
+            onClick={this.handleDelete}
+          >
+            <Link to="/">DELETAR</Link>
+          </button>
           <Link to="/">VOLTAR</Link>
         </div>
       </div>
