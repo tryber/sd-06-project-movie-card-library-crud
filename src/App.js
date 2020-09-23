7ed51a8f8b86468a17c64f4a7325dd1d5d14cb5f
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-09-23 15:34:36 -0300

    Requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MoviesDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD É ISSO AE</div>
+    <div>
+      Movie Card Library CRUD
+      <BrowserRouter>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MoviesDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="" component={NotFound} />
+      </BrowserRouter>
+    </div>
   );
 }
 
