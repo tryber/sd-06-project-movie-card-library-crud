234e3ce26057bae88d913315d47929f6dedd111f
Micaeli Teixeira <luime6@gmail.com>
2020-09-23 17:18:56 -0300

    Adicionando o componente MovieList em App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MovieList } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/" component={MovieList} />
       </Switch>
     </BrowserRouter>
   );
