2bd741276a73c76b7f1e32ea46fc852b0e8d27c5
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-23 15:34:27 -0300

    routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, EditMovie, NewMovie, NotFound } from './pages'
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route component={NotFound} />
+    </BrowserRouter>
   );
 }
 
