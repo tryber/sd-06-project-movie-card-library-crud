49599c45d0e080d7d37a7937f38cd3697bb903fd
labelle <beelsdelonge@gmail.com>
2020-09-23 15:16:15 -0300

    Resolvido rotas do EditMovie

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={MovieList} />
         <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
         <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
         <Route path="" component={NotFound} />
       </Switch>
     </BrowserRouter>
