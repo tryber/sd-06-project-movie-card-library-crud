263c79d47be9985afa4fed8cdaba1250a555d72a
Daniel Moura <darthur.moura@gmail.com>
2020-09-23 17:49:11 -0300

    Removing <Swtich> from App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
-      <Switch>
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route exact path="/" component={MovieList} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route exact path="/" component={MovieList} />
+      <Route path="*" component={NotFound} />
     </Router>
   );
 }
