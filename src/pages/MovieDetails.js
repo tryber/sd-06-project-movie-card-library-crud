0f6aed069cbb472853874634689368531592038c
CristianoNunes <cristianonunes99@gmail.com>
2020-09-23 19:58:50 -0300

    Requisito 3 -Corrigindo CodeClimate e Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
-class MovieDetails extends Component {
+export default class MovieDetails extends Component {
   constructor() {
     super();
     this.updateStateMovie = this.updateStateMovie.bind(this);
     this.state = {
-      movie: [],
+      movie: undefined,
       loading: true,
     };
   }
 
   componentDidMount() {
-    this.updateStateMovie();
+    const { id } = this.props.match.params;
+    this.updateStateMovie(id);
   }
-   
+
   updateStateMovie(id) {
     this.setState(
       { loading: true },
@@ -30,22 +32,25 @@ class MovieDetails extends Component {
   }
 
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-    const { id } = this.props;
     const { movie, loading } = this.state;
-    const { title, storyline, imagePath, genre, rating, subtitle } = {movie};
-
+    let div;
+    if (movie) {
+      const { storyline, imagePath, genre, rating, subtitle } = movie;
+      div =
+        (<div data-testid="movie-details">
+          <img alt="Movie Cover" src={`../${imagePath}`} />
+          <p>{`Subtitle: ${subtitle}`}</p>
+          <p>{`Storyline: ${storyline}`}</p>
+          <p>{`Genre: ${genre}`}</p>
+          <p>{`Rating: ${rating}`}</p>;
+        </div>);
+    }
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
+      <div>
+        {loading ? <Loading /> : div}
       </div>
     );
   }
 }
 
-export default MovieDetails;
+MovieDetails.propTypes = { match: PropTypes.shape.isRequired };
