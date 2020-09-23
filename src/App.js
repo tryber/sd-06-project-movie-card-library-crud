8ad7ad4d2769c2cd57bf465d1e81d00b59d91dc4
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-23 17:51:08 -0300

    requesito 2 alteracoes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound} from './pages';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 class App extends React.Component {
   render() {
@@ -8,7 +8,7 @@ class App extends React.Component {
       <div>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/movies/new" component={NewMovie} /> 
+            <Route exact path="/movies/new" component={NewMovie} />
             <Route exact path="/movies/:id" component={MovieDetails} />
             <Route exact path="/movies/:id/edit" component={EditMovie} />
             <Route exact path="/" component={MovieList} />
