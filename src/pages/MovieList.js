e00ce46f116e9bccb2ecda5f6e9d760762e32777
Leandro <leandro.feitoza8018@gmail.com>
2020-09-23 18:29:20 -0300

    Completado o requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
-import * as movieAPI from '../services/movieAPI';
+// import * as movieAPI from '../services/movieAPI';
+import { getMovies } from '../services/movieAPI';
+
 
 class MovieList extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: [],
-    }
+    };
+  }
+
+  componentDidMount() {
+    this.handleState();
+  }
+
+  async handleState() {
+    const moviesFromApi = await getMovies();
+    this.setState({ movies: moviesFromApi });
   }
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
+    if (movies.length === 0) return <Loading />;
 
     return (
       <div data-testid="movie-list">
