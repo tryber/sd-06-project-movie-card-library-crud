6d0fab791345379551d2d328a93cf2db56b0025d
Kesley Lahoz <namielahoz@gmail.com>
2020-09-23 15:58:38 -0300

    1 DONE!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { render } from 'react-dom'
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Switch>
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
