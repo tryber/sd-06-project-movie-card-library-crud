7d403c9d82d8c8a530eb9b12813c3e8d5614bdbe
Kesley Lahoz <namielahoz@gmail.com>
2020-09-23 16:22:12 -0300

    Fixing first CC issue

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { render } from 'react-dom'
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/" component={MovieList} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+    <div>  
+      <div>Movie Card Library CRUD</div>
+      <Router>
+        <Switch>
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/" component={MovieList} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
