fd0d47fff8be77d31f654c930e740970ef9dfe72
Isadora Koga <isadorakoga710@gmail.com>
2020-09-23 19:23:23 -0300

    Bonus ok

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -41,6 +41,7 @@ class MovieDetails extends Component {
             <p>{`Rating: ${rating}`}</p>
             <Link to="/">VOLTAR</Link>
             <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+            <Link to="/" onClick={() => movieAPI.deleteMovie(id)}>DELETAR</Link>
           </div>}
       </div>
     );
