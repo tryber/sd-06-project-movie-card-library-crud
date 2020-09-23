6a2e0181e1a84f8e650d6b9034695f1ff8cf67b1
DehSartoreto <fixsartoreto@gmail.com>
2020-09-23 17:53:47 -0300

    CC req 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
+
 class MovieDetails extends Component {
-  constructor(props) {
+  constructor() {
     super();
 
     this.state = {
       loading: true,
       movie: {},
-    }
+    };
   }
 
   componentDidMount() {
     const { id } = this.props.match.params;
     movieAPI.getMovie(id)
-    .then(response => this.setState({
+    .then((response) => this.setState({
       loading: false,
       movie: response,
-    }))
+    }));
   }
-  
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
@@ -30,27 +32,34 @@ class MovieDetails extends Component {
     const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
-        <div data-testid="movie-details">
-          {(loading) ? <Loading /> :
-          <div>
-            <section>
-              <img alt="Movie Cover" src={`../${imagePath}`} />
-              <h2>{`Title: ${title}`}</h2>
-              <p>{`Subtitle: ${subtitle}`}</p>
-              <p>{`Storyline: ${storyline}`}</p>
-              <p>{`Genre: ${genre}`}</p>
-              <p>{`Rating: ${rating}`}</p>
-            </section>
-            <section>
-              <Link to={`${id}/edit`}>EDITAR</Link>
-              <Link to={'/'}>VOLTAR</Link>
-            </section>
-          </div>
-          
-          }
+      <div data-testid="movie-details">
+        {(loading) ? <Loading /> :
+        <div>
+          <section>
+            <img alt="Movie Cover" src={`../${imagePath}`} />
+            <h2>{`Title: ${title}`}</h2>
+            <p>{`Subtitle: ${subtitle}`}</p>
+            <p>{`Storyline: ${storyline}`}</p>
+            <p>{`Genre: ${genre}`}</p>
+            <p>{`Rating: ${rating}`}</p>
+          </section>
+          <section>
+            <Link to={`${id}/edit`}>EDITAR</Link>
+            <Link to={'/'}>VOLTAR</Link>
+          </section>
         </div>
+        }
+      </div>
     );
   }
+}
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
 }
 
 export default MovieDetails;
