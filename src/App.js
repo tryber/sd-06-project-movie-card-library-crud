4d9631f1530fd03a0efc4b46c6c1c29e858a56c6
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-09-23 15:20:51 -0300

    add Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route path="/" component={NotFound} />
     </Router>
   );
 }
