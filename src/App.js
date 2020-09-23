7f65156e246fbf2c5a2cd11aabaab365336e78b4
Kesley Lahoz <namielahoz@gmail.com>
2020-09-23 18:13:36 -0300

    2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path="/movies/:id/edit" component={EditMovie} />
           <Route path="/movies/new" component={NewMovie} />
           <Route path="/movies/:id" component={MovieDetails} />
-          <Route exact path="/" render={props => <MovieList {...props} avaiableMovies={movieData} />} />
+          <Route exact path="/" component={MovieList} />
           <Route component={NotFound} />
         </Switch>
       </Router>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import movieData from '../services/movieData';
 
 class MovieCard extends React.Component {
   render() {
