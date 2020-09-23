7fc5b713c51bebf33c069544f32e402a628a8422
FlavioSugano <flavio.sugano@gmail.com>
2020-09-23 17:22:56 -0300

    Renderizaçao do MovieList.js, com a mensagem de carregado antes da requisiçao ser concluida

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -8,18 +9,31 @@ class MovieList extends Component {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     }
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  async fetchMovies() {
+    const listOfMovies = await movieAPI.getMovies();
+    this.setState({
+      loading: false,
+      movies: listOfMovies,
+    });
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+    // this.setState({loading: false});
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    const { loading, movies } = this.state;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {loading ? <Loading /> : movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
