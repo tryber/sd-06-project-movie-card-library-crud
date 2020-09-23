d2884dd4dac8ab7e45b5e03ad7f09294313ebd49
LeonardoCavachini <cavachini240@gmail.com>
2020-09-23 17:10:32 -0300

    router com erro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
+
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from'./pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+      <Switch>
+        <Router path="movies/new" component={NewMovie} />
+        <Router path="movies/:id" component={MovieDetails} />
+        <Router path="movies/:id/edit" component={EditMovie} />
+        <Router path="/" component={MovieList} />
+        <Router path="*" component={NotFound} />
+      </Switch>
+      </BrowserRouter>
   );
 }
 
