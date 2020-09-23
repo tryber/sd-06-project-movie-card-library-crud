c28352b4c1abb2f45b0ece060ad6b2f7022f63f4
labelle <beelsdelonge@gmail.com>
2020-09-23 15:13:06 -0300

    Rotas em App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages'
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="" component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
