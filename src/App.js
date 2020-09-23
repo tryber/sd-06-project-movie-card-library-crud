22de7c085875934b5e419bb8bfef0dbd2e5a1a40
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-09-23 18:44:49 -0300

    NotFound CC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { MovieList, MovieDetails, NewMovie, EditMovie } from './pages/index';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 import './App.css';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id/edit" component={EditMovie} />
-        <Route path="*" component={Notfound} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
