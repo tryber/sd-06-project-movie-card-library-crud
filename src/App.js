8268ce48cb85ec3c6d4900c0fcf6c1f2bb4e3c63
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-23 15:50:55 -0300

    Requisito 1.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+        <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route path="/" component={MovieList} />  
+      </Switch>
+      <Route component={NotFound} />
+    </BrowserRouter>
   );
 }
 
