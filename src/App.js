8da2ef58fa5fe46c440bd2b86c5d49ba84f4e23b
Macarena Rodriguez <macarenar697@gmail.com>
2020-09-23 17:20:12 -0300

    Codeclimate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import { MovieList, EditMovie, MovieDetails, NewMovie, NotFound } from "./pages";
+import { MovieList, EditMovie, MovieDetails, NewMovie, NotFound } from './pages';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-          <Route path="/" component={MovieList}/>
-          <Route path="/movies/new" component={NewMovie}/>
-          <Route path="/movies/:id" component={MovieDetails}/>
-          <Route path="/movies/:id/edit" component={EditMovie}/>
-          <Route path component={NotFound} />
+        <Route path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path component={NotFound} />
       </Router>
     );
   }
