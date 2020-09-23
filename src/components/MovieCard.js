08ff87bb709c8c59a8338b3895b1d3b21972cc71
Kesley Lahoz <namielahoz@gmail.com>
2020-09-23 18:30:53 -0300

    Fixing PropType

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const {title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div data-testid="movie-card">
-        <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
-        <div className="movie-card-body">
-          <h4 className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
+        <img src={imagePath} />
+        <div>
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
         </div>
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+  })
 }
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -11,7 +11,7 @@ class MovieList extends Component {
     }
   }
 
-   componentDidMount() {
+  componentDidMount() {
     this.fetchMovies();
   }
 
