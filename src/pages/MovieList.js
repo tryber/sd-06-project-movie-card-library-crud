ad30089196cc8f3e190b0007f90b7a10acdf0014
Anderson R <misterregisslv@gmail.com>
2020-09-23 17:51:52 -0300

    tarefa 2 concluída

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components'
+
 
 class MovieList extends Component {
   constructor() {
@@ -12,10 +14,14 @@ class MovieList extends Component {
     }
   }
 
+  async componentDidMount() {
+    const response = await movieAPI.getMovies();
+    this.setState({ movies: response });
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+   if (movies.length === 0) return <Loading />;
 
     return (
       <div data-testid="movie-list">
