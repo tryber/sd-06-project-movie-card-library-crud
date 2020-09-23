cac0b57b60d7706313236dfffe0e251aff93fa99
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-23 17:30:12 -0300

    Req 1 - App renderiza de acordo com os routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <div>My Movie Card</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/movies/new'><NewMovie /></Route>
+        <Route exact path='/movies/:id/edit'><EditMovie /></Route>
+        <Route exact path='/movies/:id'><MovieDetails /></Route>
+        <Route exact path='/'><MovieList /></Route>
+        <Route path='*'><NotFound /></Route>                
+      </Switch>
+    </BrowserRouter>
   );
 }
 
