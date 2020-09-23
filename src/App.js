e3cd1b5a70b38b1f491404554dedfba2507c9af1
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-23 17:55:56 -0300

    Starting setting the Route paths in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD!</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD!</div>
+      <Switch>
+        <Route exact path='/movies/:id/edit' component={EditMovie} />
+        <Route exact path='/movies/:id' component={MovieDetails} />
+        <Route exact path='/movies/new' component={NewMovie} />
+        <Route path='/' component={MovieList} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
