c55d88d3167c88e88414eb9f40d64da7836f20bf
Alvaro Neto <alvaroshn@gmail.com>
2020-09-23 15:16:11 -0300

    Requisito 1 - CC [1]

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages'
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/movies/new" component={NewMovie}/>
-        <Route path="/movies/:id/edit" component={EditMovie}/>
-        <Route path="/movies/:id" component={MovieDetails}/>
-        <Route path="/" component={MovieList}/>
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/" component={MovieList} />
       </Switch>
-      <Route path="" component={NotFound}/>
+      <Route path="" component={NotFound} />
     </Router>
   );
 }
