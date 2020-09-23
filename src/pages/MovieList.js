e038c301bcf403e9851ea90bbd01d591a0eeb868
RenatoCostaDev <renatotrybe@gmail.com>
2020-09-23 18:26:02 -0300

    Requisito 1 again

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
-import * as movieAPI from '../services/movieAPI';
-import Loading from '../components/Loading';
+// import * as movieAPI from '../services/movieAPI';
+// import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
