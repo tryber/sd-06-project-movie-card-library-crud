750a357f07cc302e36e16c9fd9385a84590a330b
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-23 15:46:02 -0300

    1rst Requirement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import { Loading, MovieCard, MovieForm } from './components';
+import { MovieDetails, NewMovie, EditMovie, MovieList, NotFound } from './pages'
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <switch>
+      <Router>
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/" component={MovieList} />
+        <Route path="*" component={NotFound} />
+      </Router>
+    </switch>
   );
 }
 
