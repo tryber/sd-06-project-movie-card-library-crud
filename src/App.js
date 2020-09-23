2983543c7897ee2d29e06ab6b12219fa38274ead
Pablo Santos <pablo.trybe@gmail.com>
2020-09-23 18:29:42 -0300

    Rendering first routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index.js';
 
 function App() {
   return (
-    <switch>
-      <Router>
-        <div>Movie Card Library CRUD</div>
-      </Router>
-    </switch>
-    
+    <main className="main">
+      <h1>Movie Card Library CRUD</h1>
+      <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={MovieList} />
+            <Route exact path="movies/new" component={NewMovie} />
+            <Route path="movies/:id/edit" component={EditMovie} />
+            <Route exact path="movies/:id" component={MovieDetails} />
+            <Route component={NotFound} />
+          </Switch>
+      </BrowserRouter>
+    </main>
   );
 }
 
