010b78c4619a622f5ee32d5740da6fcb302d4c1a
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-23 15:53:09 -0300

    Correções CodeClimate.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
         <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
-        <Route path="/" component={MovieList} />  
+        <Route path="/" component={MovieList} />
       </Switch>
       <Route component={NotFound} />
     </BrowserRouter>
