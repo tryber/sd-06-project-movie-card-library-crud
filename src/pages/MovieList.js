4f2533b3595d2d887d0b43c3cf66797a1200ea11
LeandroParisi <lepcbelisario@gmail.com>
2020-09-23 16:07:57 -0300

    Requisito 2 + CC 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -29,7 +29,7 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         {loading ? <Loading /> : movies.map((movie) => (
-        <MovieCard key={movie.title} movie={movie} />))}
+          <MovieCard key={movie.title} movie={movie} />))}
       </div>
     );
   }
