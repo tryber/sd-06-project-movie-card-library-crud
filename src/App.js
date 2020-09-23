afa68cc593d9b4d63b2e0edc60d61e61d54adc5d
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-23 17:04:21 -0300

    Adding BrowserRouter and Switch in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+      </Switch>
     </BrowserRouter>
   );
 }
