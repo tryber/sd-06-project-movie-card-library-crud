ea7edc73ab8b6ac52cd80f7f43f4542440cd6e04
LeandroParisi <lepcbelisario@gmail.com>
2020-09-23 16:04:18 -0300

    Requisito 2 + CC 1

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -11,11 +11,11 @@ class MovieList extends Component {
     this.state = {
       movies: [],
       loading: true,
-    }
+    };
   }
 
   componentDidMount() {
-    movieAPI.getMovies().then(result => {
+    movieAPI.getMovies().then((result) => {
       this.setState({
         loading: false,
         movies: result,
@@ -25,9 +25,11 @@ class MovieList extends Component {
 
   render() {
     const { movies, loading } = this.state;
+
     return (
       <div data-testid="movie-list">
-        {loading ? <Loading /> : movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {loading ? <Loading /> : movies.map((movie) => (
+        <MovieCard key={movie.title} movie={movie} />))}
       </div>
     );
   }
