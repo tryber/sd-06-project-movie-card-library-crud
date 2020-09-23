eb5a6166be22cf9c27b7f6b396d5e8664bc6358c
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-09-23 17:28:36 -0300

    MovieDetails structure

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      loading: false,
+    };
+  }
+
+  componentDidMount() { 
+    const { id } = this.props.match.params;
+    this.setState( { loading: true },
+      async () => {
+        this.setState({ movie: await movieAPI.getMovie(id), loading: false });
+      }
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
+    
+    if (loading) {
+      return <Loading />
+    }
 
     return (
       <div data-testid="movie-details">
+        <h4>{title}</h4>
         <img alt="Movie Cover" src={`../${imagePath}`} />
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <div className="actions">
+          <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+          <Link to="/">VOLTAR</Link>
+        </div>
       </div>
     );
   }
