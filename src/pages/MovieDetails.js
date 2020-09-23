6d597bb22e61b95e6d9c9a5a7233e0e0be98bef3
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-09-23 19:53:01 -0300

    fix more trailing spaces code climate

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -47,7 +47,7 @@ class MovieDetails extends Component {
               </button>
               <button>
                 <Link to="/">VOLTAR</Link>
-              </button>  
+              </button>
             </div>
           </div>
         }
