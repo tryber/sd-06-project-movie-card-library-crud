1744a89412cd0ae4766207c0e403f267e26e4354
HCaetano <hrcgeo@gmail.com>
2020-09-23 16:50:25 -0300

    Add the routes to 'App' component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from "react-router";
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/movies/new" component={NewMovie}></Route>
+        <Route exact path="/movies/:id" component={MovieDetails}></Route>
+        <Route path="/movies/:id/edit" component={EditMovie}></Route>
+        <Route exact path="/" component={MovieList}></Route>
+        <Route component={NotFound}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
