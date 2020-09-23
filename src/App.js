8b3e21b027cdb4e526dc77efb84d7ac70c7abde8
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2020-09-23 19:35:50 -0300

    cc1
    cc1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/';
-
-
 
 function App() {
   return (
     <div>Movie Card Library CRUD
       <BrowserRouter>
         <Switch>
-          <Route path='/movies/:id/edit' component={EditMovie}></Route>
-          <Route path='/movies/new' component={NewMovie}></Route>
-          <Route path='/movies/:id' component={MovieDetails}></Route>
-          <Route exact path='/' component={MovieList}></Route>
-          <Route path='/' component={NotFound} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/" component={MovieList} />
+          <Route path="/" component={NotFound} />
         </Switch>
       </BrowserRouter>
     </div>
