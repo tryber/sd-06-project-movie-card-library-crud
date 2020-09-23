ad9ac82cfeba1e22d84c19d4f84a24c74a4cc815
jederson <jmacedo.ti@gmail.com>
2020-09-23 17:43:28 -0300

    Requisito 02

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Switch> */}
-      <Route exact path="/" component={MovieList} />
-      <Route exact path="/movies/new" component={NewMovie} />
-      <Route exact path="/movies/:id" component={MovieDetails} />
-      <Route exact path="/movies/:id/edit" component={EditMovie} />
-      <Route path="*" component={NotFound} />
-      {/* </Switch> */}
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import { Loading } from '../components';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -7,19 +8,40 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.moviesFetch = this.moviesFetch.bind(this);
+
     this.state = {
       movies: [],
-    }
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.moviesFetch();
+  }
+
+  async moviesFetch() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const response = await movieAPI.getMovies();
+        // const responseObj = await response.json();
+        this.setState({
+          loading: false,
+          movies: response,
+        });
+      },
+    );
   }
 
   render() {
     const { movies } = this.state;
-
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {(this.state.loading) ? <Loading /> : movies.map((movie) =>
+          <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
