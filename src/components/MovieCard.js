e02a7c6c84cc18b1e85ad93f35ca8e102fd752e4
HCaetano <hrcgeo@gmail.com>
2020-09-23 19:07:30 -0300

    Add code to render 'MovieCard' component, add style to it

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './MovieCard.css';
 
 class MovieCard extends React.Component {
   render() {
+    const {
+      id,
+      title,
+      storyline,
+      imagePath,
+    } = this.props.movie;
+
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div className="movie-card" data-testid="movie-card">
+        <article>
+          <div>
+            <img src={imagePath} alt="Movie" />
+          </div>
+          <p>{ title }</p>
+          <p>{ storyline }</p>
+          <hr />
+          <Link to={`/movies/${id}`}>VER DETALHES</Link>
+        </article>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = { movie: PropTypes.objectOf(PropTypes.string).isRequired };
 
 export default MovieCard;
