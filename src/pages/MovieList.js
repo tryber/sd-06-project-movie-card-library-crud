adbc4d6885e2613bcb72c482534037043610429a
Daniel Moura <darthur.moura@gmail.com>
2020-09-23 17:09:09 -0300

    Req 2 - Rendering movie cards and loading message

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import { Loading } from '../components';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -12,10 +13,18 @@ class MovieList extends Component {
     }
   }
 
+  async componentDidMount() {
+    const moviesArray = await movieAPI.getMovies();
+    this.setState({
+      movies: moviesArray,
+    })
+  }
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (movies.length === 0) return <Loading />;
 
     return (
       <div data-testid="movie-list">
