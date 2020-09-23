2ecb04afe871ca11e7430f5adbdbd56e81e1fe0b
Thales Rangel <thalesgr@gmail.com>
2020-09-23 15:50:47 -0300

    testando para code climate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route component={NotFound} />
-      </Switch>
+      <Route path="/" component={MovieList} />
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route path="*" component={NotFound} />
     </BrowserRouter>
   );
 }
