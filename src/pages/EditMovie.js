1d8a88eeed53924eb5698ce44960a12838e2f695
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-09-23 22:53:30 -0300

    All Done

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
+
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      movie: undefined,
+      status: false,
+      shouldRedirect: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState(
+      { status: true },
+      async () => {
+        const { id } = this.props.match.params;
+        const movie = await movieAPI.getMovie(id);
+
+        this.setState({
+          movie: movie,
+          status: false,
+          shouldRedirect: false,
+        });
+      });
+
   }
 
   handleSubmit(updatedMovie) {
+    this.setState(
+      { shouldRedirect: false },
+      () => {
+        movieAPI.updateMovie(updatedMovie);
+        this.setState({
+          shouldRedirect: true,
+        });
+      });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (status) {
+      return <Loading />;
     }
 
     return (
@@ -30,5 +62,11 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    id: propTypes.number,
+  }).isRequired,
+};
 
 export default EditMovie;
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = { shouldRedirect: false };
   }
 
   handleSubmit(newMovie) {
+    this.setState(
+      { shouldRedirect: false },
+      () => {
+        movieAPI.createMovie(newMovie);
+        this.setState({
+          shouldRedirect: true,
+        });
+      });
   }
 
+
   render() {
+    if (this.state.shouldRedirect) {
+      return <Redirect to="" />;
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
