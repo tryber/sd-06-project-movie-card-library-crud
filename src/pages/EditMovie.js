86166e4c2f98bbc426c395ee7f89b5211a411d9b
Paulo <paulocheloni@gmail.com>
2020-09-24 13:28:22 -0300

    fixing lint

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
