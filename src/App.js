cc452f25bdfcb3fefdcac84db73ad259f15b779b
kissyla <kissylamottapereira@gmail.com>
2020-09-24 00:10:43 -0300

    req 1 -codeclimate

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { EditMovie, NewMovie, MovieDetails, MovieList, NotFound } from './pages/index';
-import { render } from '@testing-library/react';
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Switch>
-      <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
-      <Route path="/movies/new" render={(props) => <NewMovie {...props} />} />
-      <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
-      <Route exact path="/" render={(props) => <MovieList {...props} />} />      
-      <Route component={NotFound} />
-    </Switch>
+      <Switch>
+        <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+        <Route path="/movies/new" render={(props) => <NewMovie {...props} />} />
+        <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route exact path="/" render={(props) => <MovieList {...props} />} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
