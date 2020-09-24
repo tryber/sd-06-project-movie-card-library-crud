259021b001db894a5eb801179360dc3530b1d2ac
rafaelgelatti <rafael.gelatti@gmail.com>
2020-09-24 19:38:46 -0300

    NewMovie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -18,6 +18,7 @@ function App() {
         <Route exact path="/" component={MovieList} />
         <Route component={NotFound} />
       </Switch>
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
     </BrowserRouter>
   );
 }
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
+import { Loading } from '../components';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
@@ -7,15 +8,37 @@ class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+
+    this.state = {
+      loading: true,
+      shouldRedirect: false,
+    };
   }
 
-  handleSubmit(newMovie) {
+  componentDidMount() {
+    this.renderForm();
+  }
+
+  async handleSubmit(newMovie) {
+    await movieAPI.createMovie(newMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  renderForm() {
+    this.setState({ loading: false });
   }
 
   render() {
+    const { loading, shouldRedirect } = this.state;
+
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={this.handleSubmit} />
+        {loading ? <Loading /> : <MovieForm onSubmit={this.handleSubmit} />}
       </div>
     );
   }
