98bcc8ea80953dc3071e6923dedbaae1774bce34
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-09-24 15:31:38 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library cRuD.</div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
