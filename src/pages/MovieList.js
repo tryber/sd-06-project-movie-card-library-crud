7b0479ce8d42e1579e5f09f9fe1a23dd397caad8
Zeca Costa <jccostaso@gmail.com>
2020-09-24 12:28:59 -0300

    MovieList.js + Loading.js requisito #2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
 
-import * as movieAPI from '../services/movieAPI';
+import { Loading, MovieCard } from '../components';
+import { getMovies } from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,14 +9,33 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
-    }
+    };
+  }
+
+  // async componentDidMount() {
+  //   const movies = await getMovies();
+  //   this.setState({
+  //     movies,
+  //   });
+  // }
+
+  componentDidMount() {
+    getMovies()
+    .then((result) => {
+      this.setState({
+        movies: result,
+      });
+    });
   }
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
+    if (movies.length === 0) {
+      return (
+        <Loading />
+      );
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
