9a31ed495e3460b0019df77b3bab44a42acaca6d
Micaeli Teixeira <luime6@gmail.com>
2020-09-24 19:42:19 -0300

    cc

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -6,26 +6,26 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       shouldRedirect: false,
     };
-    
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
   async handleSubmit(newMovie) {
     await movieAPI.createMovie(newMovie);
     this.setState({ shouldRedirect: true });
   }
-  
+
   render() {
     const { shouldRedirect } = this.state;
-    
+
     if (shouldRedirect === true) {
       return <Redirect to="/" />;
     }
-    
+
     const newMovie = (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
