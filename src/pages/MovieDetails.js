0cad74bf7c9a707c39634a81045c513be447d3c9
HCaetano <hrcgeo@gmail.com>
2020-09-24 15:44:11 -0300

    Add business logic to fetch and render movie in 'MovieDetails' component

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  constructor(props) {
+    super();
 
+    this.fetchMovie = this.fetchMovie.bind(this);
+    this.disableLoadingMessage = this.disableLoadingMessage.bind(this);
+
+    this.state = {
+      receivedMovieId: props.match.params.id,
+      isLoading: true,
+      movie: {},
+    };
+  }
+
+  async componentDidMount() {
+    const movie = await movieAPI.getMovie(this.state.receivedMovieId);
+    this.disableLoadingMessage();
+    this.fetchMovie(movie);
+  }
+
+  disableLoadingMessage() {
+    this.setState({ isLoading: false });
+  }
+
+  fetchMovie(movie) {
+    this.setState({ movie });
+  }
+
+  render() {
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
+    
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
+        {
+          this.state.isLoading
+            ? <Loading />
+            : <div>
+              <img alt="Movie Cover" src={`../${imagePath}`} />
+              <p>{`Title: ${title}`}</p>
+              <p>{`Subtitle: ${subtitle}`}</p>
+              <p>{`Storyline: ${storyline}`}</p>
+              <p>{`Genre: ${genre}`}</p>
+              <p>{`Rating: ${rating}`}</p>
+            </div>
+        }
       </div>
     );
   }
