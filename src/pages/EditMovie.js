3b3ba05eab1b64e1d0a55b6664fc06da1a61791a
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-09-24 18:29:44 -0300

    Requisito5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import PropTypes from 'prop-types';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      movie: [],
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    movieAPI.getMovie(id).then((movie) =>
+      this.setState({
+        movie,
+        status: false,
+      })
+    );
+  }
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie).then(() =>
+      this.setState({
+        shouldRedirect: true,
+      })
+    );
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
+    const { history } = this.props;
     if (shouldRedirect) {
-      // Redirect
+      history.push('/'); // Redirect
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +49,16 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default EditMovie;
