5ef8fcdd6fb20c0365f60e2762f3de7204668b6a
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-24 15:39:19 -0300

    Correções CodeClimate.

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import EditMovie from './EditMovie';
 
 class MovieDetails extends Component {
   constructor() {
