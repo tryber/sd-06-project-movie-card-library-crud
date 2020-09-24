838e051b49586adaae0b2f7b692f33bc1438ef85
paulo-lins <paulolins100@gmail.com>
2020-09-24 15:20:46 -0300

    MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
+// import MovieDetails from '../pages/MovieDetails';
 class MovieCard extends React.Component {
+
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <span key={movie.title} />
+        <h4>{movie.title}</h4>
+        <p>{movie.storyline}</p>
+        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
       </div>
     );
   }
 }
 
 export default MovieCard;
+
+
+// 3 - MovieCard: deve possuir um link para a página de detalhes de um filme
+// ✕ deve exibir pelo menos o título e a sinopse de seu respectivo filme (65ms)
+// ✕ deve conter um link com o texto "VER DETALHES" que redireciona para a página de detalhes do filme (12ms)
