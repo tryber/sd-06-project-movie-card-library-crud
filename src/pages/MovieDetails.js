380abb295178591d077289b3f20a9ca88ea07bac
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-24 00:47:06 -0300

    Bonus requirement

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -54,6 +54,9 @@ class MovieDetails extends Component {
           <p>{`Rating: ${rating}`}</p>
           <Link to={`/movies/${id}/edit`}>EDITAR</Link>
           <Link to="/">VOLTAR</Link>
+          <Link to="/" onClick={() => { movieAPI.deleteMovie(id); }}>
+            DELETAR
+          </Link>
         </div>
         }
       </div>
