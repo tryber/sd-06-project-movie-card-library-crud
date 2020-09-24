485342c5114e9c43adb3d1224f0e90b9808c233b
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-24 15:57:52 -0300

    CC, MovieCard propTypes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,10 +11,21 @@ class MovieCard extends React.Component {
       <div data-testid="movie-card">
         <h1>{movie.title}</h1>
         <p>{movie.storyline}</p>
-          <Link to={detailsPath}>VER DETALHES</Link>
+        <Link to={detailsPath}>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = { movie: PropTypes.shape({
+  id: PropTypes.number,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  imagePath: PropTypes.string,
+  bookmarked: PropTypes.bool,
+  genre: PropTypes.string,
+})}.isRequired;
 
 export default MovieCard;
