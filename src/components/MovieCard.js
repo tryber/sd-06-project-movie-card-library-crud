04b80e84e7fb0e7898151b40d000b369e6b7356f
higoramorim <1810042@aluno.univesp.br>
2020-09-23 22:45:29 -0300

    requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+/*
+Todos os MovieCards devem possuir em seu conteúdo, pelo menos, 
+o título, a sinopse e um link com o texto "VER DETALHES" que 
+aponta para a rota movies/:id, onde :id é o id do filme. 
+Esta rota exibirá informações detalhadas de um filme.
+
+O que será testado:
+cada MovieCard deve exibir pelo menos o título e a sinopse de seu respectivo filme
+cada MovieCard deve conter um link com o texto VER DETALHES que redireciona para a 
+página de detalhes do filme
+*/
 
 class MovieCard extends React.Component {
-  render() {
+  render() {  
+  const { id, title, subtitle, rating, storyline, imagePath } = this.props.movie;
+  
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} alt="Movie Cover" />
+        <h3>{id}</h3>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>{storyline}</p>
+        <p>{rating}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
