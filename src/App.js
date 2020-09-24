b24e2c2dc4578ce37dba19eb682c96d6cd6d8c8b
VinicinBraga <vinicius@bragamatta.com.br>
2020-09-24 19:42:11 -0300

    Implementando Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
@@ -7,11 +7,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/movies/new" component={NewMovie} />
+        <Route exact path="/" component={MovieList} />
         <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/" component={MovieList} />
-        <Route path="" component={NotFound} />
+        <Route path="/" component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,28 @@
+
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 
 class MovieCard extends React.Component {
   render() {
+    const { title, storyline, id, imagePath } = this.props.movie;
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div data-testid='movie-card'>
+        <img src={imagePath}/>
+        <h3>{title}</h3>
+        <p>{storyline}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
