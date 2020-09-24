7644dd880d5fe37548779b0868ea9935079df4a8
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-09-24 17:58:18 -0300

    requisito 3 cc2

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,13 +5,12 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, id, imagePath } = movie;
+    const { title, storyline, id, imagePath } = movie;
     return (
       <div data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
         <div className="movie-card-body">
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
           <Link to={`/movies/${id}`}>VER DETALHES</Link>
         </div>
@@ -22,5 +21,11 @@ class MovieCard extends React.Component {
 
 export default MovieCard;
 
-MovieCard.defaultProps = { movie: {} };
-MovieCard.propTypes = { movie: PropTypes.object };
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired
+  }).isRequired,
+};
