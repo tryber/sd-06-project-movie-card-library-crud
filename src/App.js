0fdc7a6277aaedfad4e2745cd475e00de6fcc907
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-24 18:59:16 -0300

    Codeclimate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import MovieList from './pages/MovieList'
-import MovieDetails from './pages/MovieDetails'
-import NewMovie from './pages/NewMovie'
-import EditMovie from './pages/EditMovie'
-import NotFound from './pages/NotFound'
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
