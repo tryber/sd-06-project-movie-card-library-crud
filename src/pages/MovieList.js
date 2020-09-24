29db70766957c60fc68a034f2e80ec2197313871
William Gomes <williamjog@hotmail.com>
2020-09-24 10:22:42 -0300

    2nd requirement | Added componentDidMont with movieAPI

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading'
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,17 +9,23 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     }
   }
 
+  async componentDidMount() {
+    const arrayMovies = await movieAPI.getMovies();
+    this.setState({
+      movies: arrayMovies,
+      loading: false,
+    });
+  }
+  
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        (loading === true ?) <Loading /> : {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
