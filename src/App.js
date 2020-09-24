ae84f1d8cd583e1b8e601aac24b2798f5026cdd1
VinicinBraga <vinicius@bragamatta.com.br>
2020-09-24 17:37:58 -0300

    Implementando Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/" component={MovieList}/>
+      </Switch>
+      <Route path="" component={NotFound} />
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App; 
