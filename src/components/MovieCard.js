f2fd54ee59bbac8b081c45da31c069950d671fea
VinicinBraga <vinicius@bragamatta.com.br>
2020-09-24 19:49:09 -0300

    Implementando Requisito 3 - Code Climate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,13 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     const { title, storyline, id, imagePath } = this.props.movie;
     return (
-      <div data-testid='movie-card'>
-        <img src={imagePath}/>
+      <div data-testid="movie-card">
+        <img src={imagePath} alt="Movie Image" />
         <h3>{title}</h3>
         <p>{storyline}</p>
         <Link to={`/movies/${id}`}>VER DETALHES</Link>
