146d309252a1d9864e2357b56348dcd4fab691ff
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-09-24 18:57:22 -0300

    Requisito Bonus CodeClimate 05

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -68,7 +68,7 @@ class MovieDetails extends Component {
 
 MovieDetails.propTypes = {
   match: PropTypes.shape({
-    params: PropTypes.shape({ id: PropTypes.number }),
+    params: PropTypes.shape({ id: PropTypes.number.isRequired }),
   }),
 };
 
