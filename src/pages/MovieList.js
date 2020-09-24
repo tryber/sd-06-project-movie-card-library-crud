8768b1e1fbb49ae93dbeb295f9cda80665d9e3b0
Micaeli Teixeira <luime6@gmail.com>
2020-09-24 19:15:52 -0300

    Adicionando o link para adicionar cartão

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { MovieCard, Loading } from '../components';
-
 import * as movieAPI from '../services/movieAPI';
+import { Link } from 'react-router-dom';
 
 class MovieList extends Component {
   constructor() {
@@ -33,6 +33,8 @@ class MovieList extends Component {
     const movieList = (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {<Link to='/movies/new'>ADICIONAR CARTÃO</Link>}
+       
       </div>
     );
 
