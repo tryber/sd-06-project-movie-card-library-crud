c28536fe363b152f9394e3384b532e96a9db218c
Kesley Lahoz <namielahoz@gmail.com>
2020-09-24 12:49:05 -0300

    Requisito 3, sera que vai passar sem dar erro no CC?

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, id } = this.props.movie;
     return (
       <div data-testid="movie-card">
         <img alt="Folder"src={imagePath} />
@@ -12,6 +13,7 @@ class MovieCard extends React.Component {
           <h5>{subtitle}</h5>
           <p>{storyline}</p>
         </div>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
