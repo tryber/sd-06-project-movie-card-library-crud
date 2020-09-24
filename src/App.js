bbd23dc71b35612dfb5d8cc603f34ecbea8d3fec
luiz <phellipe501@gmail.com>
2020-09-23 21:03:06 -0300

    Criando outra branch - Luiz Phellipe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { EditMovie, NewMovie, MovieDetails, MovieList, NotFound } from './pages';
+import './App.css';
+import './MovieCard.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Switch>
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+        <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
