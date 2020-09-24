6bf8fde42b10d04cdaae5ae9f23646436865b192
Anderson R <misterregisslv@gmail.com>
2020-09-24 03:18:08 -0300

    tarefa 3,4,5 concluida

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    console.log(props);
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: '',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeSetState = this.changeSetState.bind(this);
+  }
+  
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const response = await movieAPI.getMovie(id);
+    this.changeSetState(response);
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  changeSetState(componentResponse) {
+    this.setState({
+      movie: componentResponse,
+      status: '',
+    });
   }
 
   render() {
+    console.log(this.state);
+    console.log(this.props.match.params);
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieDetails extends Component {
+
+  constructor() {
+    super();
+
+    this.changeSetState = this.changeSetState.bind(this);
+
+    this.state = {
+      movieDetailsSelected: '',
+    };
+  }
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const response = await movieAPI.getMovie(id);
+    this.changeSetState(response);
+  }
+
+  changeSetState(componentResponse) {
+    this.setState({ movieDetailsSelected: componentResponse });
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    if (this.state.movieDetailsSelected === '') return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const {
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle,
+      id,
+    } = this.state.movieDetailsSelected;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Título: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
