870c1348f65d95bda481cb1caf726c73920de62e
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-09-24 19:07:47 -0300

    Updating Branch

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-
+import PropTypes from 'prop-types';
 import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
@@ -55,5 +55,7 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {match: PropTypes.object.isRequired}
 
 export default EditMovie;
