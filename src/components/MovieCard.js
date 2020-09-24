344f310923a6c7ab6dbd8e88afea0459fadbb0de
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-24 16:28:22 -0300

    requesito 3 alteracoes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-  const { title, storyline, id } = this.props.movie;
+    const { movie: title, storyline, id } = this.props;
 
     return (
       <div data-testid="movie-card">
