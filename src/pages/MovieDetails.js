5e6d4f6da8f7599a6e571a2e887804183a0676a8
FlavioSugano <flavio.sugano@gmail.com>
2020-09-24 15:28:33 -0300

    MovieDetails.js: links p/ voltar p/ a Home e p/ ir p/ a pagina de ediçao, adiçao do titulo p/ ser renderizado

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
@@ -29,7 +29,7 @@ class MovieDetails extends Component {
 
   render() {
     const { loading, movie } = this.state;
-    const { storyline, imagePath, genre, rating, subtitle } = movie;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     if (loading) {
       return <Loading />;
@@ -38,10 +38,13 @@ class MovieDetails extends Component {
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h3>{`Title: ${title}`}</h3>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={'/'}>VOLTAR</Link>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
@@ -50,7 +53,7 @@ class MovieDetails extends Component {
 MovieDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.number,
+      id: PropTypes.string,
     }),
   }).isRequired,
 };
