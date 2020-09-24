032b1e1e3dab779b65e142b8395d9cfb14546682
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-09-24 19:23:43 -0300

    quebrei e arrumei

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom;'
+import { Link } from 'react-router-dom';
 import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
