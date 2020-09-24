049a9a196a9b26def0746078ef8fcecefbb7b139
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-24 00:30:26 -0300

    5th requirement 02_fixing CC issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
 import { MovieDetails, NewMovie, EditMovie, MovieList, NotFound } from './pages';
 
 function App() {
