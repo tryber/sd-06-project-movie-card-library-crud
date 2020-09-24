5be64cc8b1f2b15b365ad5d2b617c891bc0fbde7
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-24 20:20:15 -0300

    requesito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.fatchMovie = this.fatchMovie.bind(this);
+
+    this.state = {
+      id: 0,
+      movie: {},
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    this.fatchMovie();
+  }
+
+  async fatchMovie() {
+    const { id } = this.props.match.params;
+    this.setState({
+      movie: await movieAPI.getMovie(id),
+      loading: false,
+      id: id,
+    })
+  }
+
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = this.state.movie;
+
+    const { loading } = this.state;
+
+    if (loading === true) return <Loading />
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
+      <div>
+        <div data-testid="movie-details">
+          <img alt="Movie Cover" src={`../${imagePath}`} />
+          <p>{`Title: ${title}`}</p>
+          <p>{`Subtitle: ${subtitle}`}</p>
+          <p>{`Storyline: ${storyline}`}</p>
+          <p>{`Genre: ${genre}`}</p>
+          <p>{`Rating: ${rating}`}</p>
+          <div>
+            <button><Link to={`/movies/${id}/edit`}>EDITAR</Link></button>
+            <button><Link to=''>VOLTAR</Link></button>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  id: PropTypes.number.isRequired,
+};
 
 export default MovieDetails;
