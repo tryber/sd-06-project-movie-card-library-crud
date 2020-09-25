3d9397d2b76629169d5269512edd9d4545877b7c
Anderson <andersoni.trybe@gmail.com>
2020-09-25 15:51:34 -0300

    Construção basica do MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+
+    const{ title, storyline, id, imagePath } = this.props;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h4>{title}</h4>
+        <img source={imagePath} />
+        <p>{storyline}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = { movie: PropTypes.shape({
+  title: PropTypes.string,
+  storyline: PropTypes.string,
+  id: PropTypes.number,
+  imagePath: PropTypes.string,
+}).isRequired };
 
 export default MovieCard;
