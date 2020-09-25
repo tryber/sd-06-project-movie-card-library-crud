3fa1f6f9d1377ec8aef2c182da6144d90446cba8
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-09-25 15:57:10 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Switch>
         <Route path="/movies/:id/edit" render={() => <EditMovie />} />
         <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id" render={() => <MovieDetails />} />
+        <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
         <Route exact path="/" render={() => <MovieList />} />
         <Route path="*" component={NotFound} />
       </Switch>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: [],
+      loading: true,
+    }
+  }
+  componentDidMount() {
+
+    movieAPI.getMovie(this.props.match.params.id).then((answer) => {
+      this.setState({
+        movie: answer,
+        loading: false,
+      })
+    })
+  }
+
+  render() {
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
+    const { loading } = this.state;
+
+    if (loading === true) {
+      return <Loading />;
+    } 
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <hr />
+        <Link to="/">VOLTAR</Link><br />
+        <Link to="/movies/:id/edit">EDITAR</Link>
       </div>
     );
   }
 }
 
 export default MovieDetails;
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    id: PropTypes.number,
+  }).isRequired,
+};
+
