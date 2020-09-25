bfb4cede6a5780cb67b16494ce69120a20807941
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-24 22:10:22 -0300

    add MovieList elements

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import Loading from '../components/Loading';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -9,20 +11,35 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true, 
     }
   }
 
+  componentDidMount() {
+    this.renderMovies()
+  }
+  
+  async renderMovies() {
+    const request = await movieAPI.getMovies();
+    this.setState({ movies: request, loading: false });
+  }
+  
   render() {
+    
     const { movies } = this.state;
-
+    const { loading } = this.state;
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {loading ? <Loading /> :
+          movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)
+        }
+
       </div>
     );
   }
 }
 
 export default MovieList;
+// movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)
