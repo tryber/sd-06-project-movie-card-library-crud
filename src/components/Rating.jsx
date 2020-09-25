61c41b140aab9a7bc501dac9100b9d870c4f8acf
William Gomes <williamjog@hotmail.com>
2020-09-25 15:45:18 -0300

    CodeClimate 3

---
+++ src/components/Rating.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <label className="label">Rating:</label>
+        <span className="rating">{rating}</span>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = PropTypes.number.isRequired;
+
+export default Rating;
