b105c6a8acc429f1a8806cc5f952e94c938672cc
Anderson <andersoni.trybe@gmail.com>
2020-09-25 15:29:38 -0300

    singlequote fix for CodeClimate

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
-
-
 function App() {
   return (
     <BrowserRouter>
       <h1>MCL CRUD</h1>
       <Switch>
-        <Route exact path='/' component={MovieList} />
-        <Route path='/movies/:id' component={MovieDetails} />
-        <Route path='/movies/new' component={NewMovie} />
-        <Route path='/movie/:id/edit' component={EditMovie} />
+        <Route exact path="/"component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movie/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
