38c7aa2410fba6a818fbdb9f9d97915976da38ed
Julia Porto <juportob@gmail.com>
2020-09-25 19:08:13 -0300

    5th requirement

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
     <Router>
       <div>Movie Card Library CRUD</div>
       <Route exact path="/" component={MovieList} />
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="/movies/:id" component={MovieDetails} />
-      <Route path="/movies/:id/edit" component={EditMovie} />
-      <Route path="/:notfound" component={NotFound} />
+      <Route exact path="/movies/new" component={NewMovie} />
+      <Route exact path="/movies/:id" component={MovieDetails} />
+      <Route exact path="/movies/:id/edit" component={EditMovie} />
+      <Route exact path="/:notfound" component={NotFound} />
     </Router>
   );
 }
