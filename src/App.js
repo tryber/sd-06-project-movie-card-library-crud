42fade9181770d515f13ba677de0b278344bfb5d
Alessandra <itsalematos@hotmail.com>
2020-09-25 14:18:27 -0400

    Creating routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-//commit 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import './App.css';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages'
+
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route path="" component={NotFound} />
+    </Router>
   );
 }
 
