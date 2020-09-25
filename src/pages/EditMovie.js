e7714de039a0037340efeefd7617cd92d94d0f0d
FlavioSugano <flavio.sugano@gmail.com>
2020-09-25 16:58:06 -0300

    EditMovies: renderizar 'Carregando... ', alimentar o form c/ as infos atuais do filme e o submit altera o movieData.js; depois disso, redireciona p/ a Home

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      id: props.match.params.id,
+      loading: true,
+      shouldRedirect: false,
+      movie: '',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  async fetchMovie() {
+    const choosenMovie = await movieAPI.getMovie(this.state.id);
+    this.setState({
+      loading: false,
+      movie: choosenMovie,
+    });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { loading, shouldRedirect, movie } = this.state;
+
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (loading) {
+      return <Loading />
     }
 
     return (
