f651f897f76890d7255ead9b4c3153522ec99d4a
ilanribeiro <ilanribeiro@gmail.com>
2020-09-25 03:08:24 -0300

    requirement 3 completed - MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { imagePath, title, storyline, id } = this.props.movie;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} alt="Movie cover" />
+        <h3>{title}</h3>
+        <p>{storyline}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
+        {/* Solution above learned here:
+        https://stackoverflow.com/questions/35086162/react-render-link-containing-a-variable */}
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
