90354ecc0c3cc10adc7c173d418be9f2c5ad526a
Igor Leão <igorleao.eng@gmail.com>
2020-09-25 15:59:13 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
+
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-      <Switch>
+        <Switch>
         <Route exact path="/movies/:id/edit" component={EditMovie} />
         <Route exact path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/" component={MovieList} />
         <Route path="*" component={NotFound} />
       </Switch>
-    </BrowserRouter>
+      </BrowserRouter>
     );
   }
 
