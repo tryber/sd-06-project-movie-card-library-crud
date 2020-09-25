349f24179ff4ca2fd4ca92bd773c83d38803ef4c
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-25 17:24:49 -0300

    Adding properties status, shouldRedirect and movie  in state, inside constructor from EditMovie component

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -6,7 +6,13 @@ import * as movieAPI from '../services/movieAPI';
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: [],
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
