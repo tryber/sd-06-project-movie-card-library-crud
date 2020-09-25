597b11d4d6026d12eb3612e3f57bd29e330fd8f3
Anderson <andersoni.trybe@gmail.com>
2020-09-25 16:29:43 -0300

    MovieDetails construido

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,21 +2,48 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = { movie: [] }
+  }
+
+  componentDidMount(){
+    this.getMovieData()
+  }
+
+  async getMovieData() {
+    const id = this.props.match.params.id;
+    const fetchData = await movieAPI.getMovie(id);
+    this.setState({ movie: fetchData });
+  }
+
   render() {
+
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
     // Change the condition to check the state
     // if (true) return <Loading />;
+    while(movie.length === 0) {
+      return <Loading />
+    }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <div>
+          <button><Link to={`/movies/${id}/edit`} >EDITAR</Link></button>
+          <button><Link to="/">VOLTAR</Link></button>
+        </div>
       </div>
     );
   }
