a430bddb9089d8ad28cecd7be60a3887551647be
Julia Porto <juportob@gmail.com>
2020-09-25 15:25:01 -0300

    Adding first requirement - CC

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Route path="/movies/new" component={NewMovie} />
       <Route path="/movies/:id" component={MovieDetails} />
       <Route path="/movies/:id/edit" component={EditMovie} />
-      <Route path="" component={NotFound} />
+      <Route path="/:notfound" component={NotFound} />
     </Router>
   );
 }
