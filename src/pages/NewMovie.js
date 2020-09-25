9d3c637f79c99953b078a97675e3caabb43ba60e
Leandro <leandro.feitoza8018@gmail.com>
2020-09-24 22:29:26 -0300

    Feito o requisito 6

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
-import { createMovie, getMovie } from '../services/movieAPI'
+import { createMovie } from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidUpdate() {
-    this.handleSubmit()
+    this.state = {
+      redirect: false,
+    };
   }
 
   async handleSubmit(newMovie) {
-    const result = await getMovie();
-    console.log(await result)
+    await createMovie(newMovie);
+    this.setState({ redirect: true });
   }
 
   render() {
+    const { redirect } = this.state;
+    if (redirect === true) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
@@ -27,14 +31,3 @@ class NewMovie extends Component {
   }
 }
 export default NewMovie;
-
-// async handleFetchMovieApi() {
-//   const result = await getMovie(this.props.match.params.id);
-//   this.setState({
-//     movie: result,
-//     loading: true,
-//   });
-// }
-
-// await createMovie(newMovie)
-//     return <Redirect to="/" />;
