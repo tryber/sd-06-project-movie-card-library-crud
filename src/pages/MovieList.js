fb79137a0af277981474c8c7c0d358b9bf8719da
Ana Higo <anahigo.arq@gmail.com>
2020-09-25 15:53:50 -0300

    Requisito 2, Ok! cc again 3?

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -35,7 +35,7 @@ class MovieList extends Component {
     if (loading) return <Loading />;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => { return <MovieCard key={movie.title} movie={movie} /> })}
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
         <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
