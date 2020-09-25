b8dbf01fab4629e24efce49ef6057aa04691ac1b
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-25 16:13:17 -0300

    Adding movie information to be rendered

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" className="movie-card-image" src={movie.imagePath} />
+        <div className="movie-card-body">
+          <h4 data-testid="movie-card-title" className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+          <div>
+          </div>
+          <div>
+            <h4 className="movie-card-rating">{movie.rating}</h4>
+          </div>
+        </div>
       </div>
     );
   }
