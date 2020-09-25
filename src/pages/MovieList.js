375c151fcc2b23c570e83c79dd1e7adea3c2b1ea
ilanribeiro <ilanribeiro@gmail.com>
2020-09-25 02:25:55 -0300

    requirement 2 completed - modified location of setState because codeclimate warning

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
-import { getMovies } from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 import Loading from '../components/Loading';
 
@@ -11,24 +11,33 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
-      loading: true,
     };
   }
 
-  async componentDidMount() {
-    const movies = await getMovies();
-    this.setState({ movies, loading: false });
+  componentDidMount() {
+    this.fetchMovies();
   }
 
+  async fetchMovies() {
+    const movies = await movieAPI.getMovies();
+    this.setState({ movies });
+  }
+
+
   render() {
-    const { movies, loading } = this.state;
+    const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (movies.length === 0) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-        { loading ? <Loading /> : '' }
+        {/* { loading ? <Loading /> : '' } */}
+        {/* solution above learned here:
+        https://www.basefactor.com/react-how-to-display-a-loading-indicator-on-fetch-calls */}
       </div>
     );
   }
