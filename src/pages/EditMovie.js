d48b85e317354b586205ec06a1975168612ad532
Anderson <andersoni.trybe@gmail.com>
2020-09-25 18:46:26 -0300

    Ajustes do CodeClimate

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
-import { Redirect } from 'react-router-dom';
 
 class EditMovie extends Component {
   constructor(props) {
