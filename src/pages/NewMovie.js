31781f00926cfdd737cd561dd6eabd7297474a24
VinicinBraga <vinicius@bragamatta.com.br>
2020-09-25 18:00:56 -0300

    Implementando Requisito 6 - Code Climate

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
-class NewMovie extends Component {
+class NewMovie extends Component { 
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
 
     this.state = {
       shouldRedirect: false,
-    };  
+    };
   }
 
   async handleSubmit(newMovie) {
     const addmovie = await movieAPI.createMovie(newMovie);
     if (addmovie === 'Ok') {
       this.setState({
-      shouldRedirect: true,
+        shouldRedirect: true,
       });
     }
     this.setState({ shouldRedirect: true });
   }
- 
+
   render() {
-      const { shouldRedirect } = this.state;
-      if (shouldRedirect) {
-        return (<Redirect to="/" />);
-      }
-      return (
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) {
+      return (<Redirect to="/" />);
+    }
+    return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
       </div>
     );
   }
 }
+
 export default NewMovie;
