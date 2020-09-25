9c82c8c7f73b119e9d3f1a21a77c501303b81768
Alessandra <itsalematos@hotmail.com>
2020-09-25 15:31:22 -0400

    Showing load message

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,30 +9,29 @@ class MovieList extends Component {
     this.handleFetch = this.handleFetch.bind(this);
     this.state = {
       movies: [],
-      loading: true
+      loading: true,
     }
-  }
-
-  async handleFetch () {
-    const result = await movieAPI.getMovies();
-    this.setState({ 
-      movies: result,
-      loading: false });
   }
 
   componentDidMount() {
     this.handleFetch();
   }
 
+  async handleFetch() {
+    const result = await movieAPI.getMovies();
+    this.setState({
+      movies: result,
+      loading: false });
+  }
+
   render() {
     const { movies, loading } = this.state;
 
-    // Render Loading here if the request is still happening
-    // if (movies === []) return Loading;
     return (
 
       <div data-testid="movie-list">
-        {loading ? <Loading /> : movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {loading ? <Loading /> : movies.map((movie) =>
+        <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
