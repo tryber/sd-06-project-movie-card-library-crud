a40aba3b12b89344207171371e8e5bc67dea8cb3
paulo-lins <paulolins100@gmail.com>
2020-09-24 21:08:05 -0300

    moviecard

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -52,9 +52,9 @@ class MovieDetails extends Component {
 MovieDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.number,
-    }),
-  }),
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default MovieDetails;
