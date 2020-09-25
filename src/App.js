45f61b7f92cb19795d52e16d90fa3f408d407d3d
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-25 18:41:28 -0300

    Renderizado o componente BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD - #GOTRYBE</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD - #GOTRYBE</div>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route
+          path="/movies/:id/edit"
+          render={(props) => <EditMovie {...props} />}
+        />
+        <Route
+          path="/movies/:id"
+          render={(props) => <MovieDetails {...props} />}
+        />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
