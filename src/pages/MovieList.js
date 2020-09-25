3764a35f0e5d539103eac42914c134a459b40794
Alessandra <itsalematos@hotmail.com>
2020-09-25 15:29:03 -0400

    Showing load message

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading'
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
+    this.handleFetch = this.handleFetch.bind(this);
     this.state = {
       movies: [],
+      loading: true
     }
   }
 
+  async handleFetch () {
+    const result = await movieAPI.getMovies();
+    this.setState({ 
+      movies: result,
+      loading: false });
+  }
+
+  componentDidMount() {
+    this.handleFetch();
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    // if (movies === []) return Loading;
     return (
+
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {loading ? <Loading /> : movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
