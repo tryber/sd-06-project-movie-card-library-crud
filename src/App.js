5007cc07d5dd4a23699e9b03c02eb62443f19abd
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-09-24 21:10:54 -0300

    Implementation of Task 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList.js';
+import MovieDetails from './pages/MovieDetails.js';
+import NewMovie from './pages/NewMovie.js';
+import EditMovie from './pages/EditMovie.js';
+import NotFound from './pages/NotFound.js';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/page-not-found" component={NotFound} />
+        <Route exact path="/" component={MovieList} />
+        <Redirect to="/page-not-found" />
+      </Switch>
     </Router>
   );
 }
