5ec2757a122162b7cfd03a5fb0cd238b705ee4cc
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-09-25 00:31:58 -0300

    Implementation of Task 2

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './MovieCard.css';
+
 
 class MovieCard extends React.Component {
   render() {
+    const { title, imagePath: imgSrc, subtitle, storyline, id, rating, bookmarked, genre} = this.props.movie;
+
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div className="movie-card" data-testid="movie-card">
+        <img src={imgSrc} />
+        <h1>{title}</h1>
+        <p>{storyline}</p>
+        <p className="see-details">VER DETALHES</p>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
+import './MovieList.css';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,19 +11,27 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: '',
     }
   }
 
   render() {
     const { movies } = this.state;
-
     // Render Loading here if the request is still happening
-
+    if (this.state.loading) return <Loading />;
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      <div className="movie-list-container" data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.setState({loading: true},
+    () => movieAPI.getMovies().then(response => this.setState(() => {
+      const newMovies = response;
+      return ({movies: newMovies, loading: false});
+    })));
   }
 }
 
