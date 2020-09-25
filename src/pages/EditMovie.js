190fc1174720d02d3888e6692b1534a7d02b29c4
HCaetano <hrcgeo@gmail.com>
2020-09-25 16:20:26 -0300

    Remove unnecessary commented lines from file

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
 
 class EditMovie extends Component {
   constructor(props) {
@@ -27,16 +28,16 @@ class EditMovie extends Component {
     this.setState({ movie, isLoading: false });
   }
 
-  handleSubmit(updatedMovie) {
-    movieAPI.updateMovie(updatedMovie);
-    this.props.history.push('/');
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
   }
 
   render() {
     const { isLoading, shouldRedirect, movie } = this.state;
 
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect path="/"/>
     }
 
     if (isLoading) {
