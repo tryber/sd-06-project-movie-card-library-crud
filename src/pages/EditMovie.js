dfd834c2d4711877d473c1438401da639578d44d
labelle <beelsdelonge@gmail.com>
2020-09-25 17:18:53 -0300

    Terminando requisitos obrigatorios 100%  com CC

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: '',
+      loading: true,
+      redirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    movieAPI.getMovie(id)
+    .then((movie) => this.setState({ movie, loading: false }));
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie)
+    .then(() => this.setState({ loading: false, redirect: true }));
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
-
-    if (status === 'loading') {
-      // render Loading
-    }
+    const { loading, movie, redirect } = this.state;
+    if (loading) return <Loading />;
+    if (redirect) return <Redirect to="/" />;
 
     return (
       <div data-testid="edit-movie">
@@ -30,5 +38,11 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.object,
+  }).isRequired,
+};
 
 export default EditMovie;
