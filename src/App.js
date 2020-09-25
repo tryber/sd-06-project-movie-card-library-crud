8479aad05476d78bd37b824e029c41f96afd2936
rodrigoderkian <derkianpromo@gmail.com>
2020-09-25 18:28:10 -0300

    Alterando o texto de Loading

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages'
 
 function App() {
   return (
-      <BrowserRouter>
-      <Switch>
-      <Route path="/movies/:id" component={MovieDetails} />
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="/movies/:id/edit" component={EditMovie} />
-      <Route path="" component={NotFound} />
-      <Route path="/" component={MovieList} />
-      </Switch>
+    <BrowserRouter>
+    <Switch>
+    <Route path="/movies/:id" component={MovieDetails} />
+    <Route path="/movies/new" component={NewMovie} />
+    <Route path="/movies/:id/edit" component={EditMovie} />
+    <Route path="" component={NotFound} />
+    <Route path="/" component={MovieList} />
+    </Switch>
     </BrowserRouter>
   );
 }
