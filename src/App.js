5a7f316a2baba28bd049459147ede911653f195a
Bruna Franciele <brunafranciele1902@gmail.com>
2020-09-25 15:18:35 -0300

    primeiro requisito - consertando cc

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages'
 function App() {
   return (
     <Router>
-    <div>Movie Card Library CRUD</div>
-    <Route exact path="/" component={MovieList} />
-    <Route exact path="/movies/:id" component={MovieDetails} />
-    <Route exact path="/movies/new" component={NewMovie} />
-    <Route exact path="/movies/:id/edit" component={EditMovie} />
-    <Route exact path="" component={NotFound} />
+      <div>Movie Card Library CRUD</div>
+      <Route exact path="/" component={MovieList} />
+      <Route exact path="/movies/:id" component={MovieDetails} />
+      <Route exact path="/movies/new" component={NewMovie} />
+      <Route exact path="/movies/:id/edit" component={EditMovie} />
+      <Route exact path="" component={NotFound} />
     </Router>
   );
 }
