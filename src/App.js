14fa532f652c9b4c0fc0f640896fbb7d0fc80180
rodrigoderkian <derkianpromo@gmail.com>
2020-09-25 17:46:58 -0300

    Adicionando exact no route path /

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages'
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={MovieList} />
       <Route path="/movies/:id" component={MovieDetails} />
       <Route path="/movies/new" component={NewMovie} />
       <Route path="/movies/:id/edit" component={EditMovie} />
-      <Route path="" component={NotFound} />
+      <Route component={NotFound} />
+      <Route exact path="/" component={MovieList} />
     </BrowserRouter>
   );
 }
