25a1e44632fe363c929cfedac92c672464fdfdbd
HCaetano <hrcgeo@gmail.com>
2020-09-25 17:46:49 -0300

    Remove link from 'MovieList' component

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { MovieCard, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
@@ -38,10 +37,7 @@ class MovieList extends Component {
         {
           this.state.isLoading
             ? <Loading />
-            : <div>
-              { movies.map((movie) => <MovieCard key={movie.title} movie={movie} />) }
-              <Link to="/movies/new">ADICIONAR CARTÃO</Link>
-            </div>
+            : movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)
         }
       </div>
     );
