44f860f07c79c9abd4ef25dd25df4797fd9d5e4b
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-09-24 21:19:47 -0300

    MovieDetails 02

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -33,7 +33,7 @@ class MovieDetails extends Component {
     const { movie, isLoading } = this.state;
     if (isLoading) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
