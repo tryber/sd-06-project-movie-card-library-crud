c7b0d962c6021a707950f3b8d1c13d722707bce4
vbergadev <vbergadev@gmail.com>
2020-09-26 20:23:51 -0300

    requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import movies from '../services/movieData';
 
 class MovieDetails extends Component {
   constructor() {
     super();
 
     this.state = {
-      movies: [],
+      movie: {},
+      loading: true,
     };
   }
+  componentDidMount() {
+    return this.fetchAPIMovie()
+  }
+  async fetchAPIMovie() {
+    const apiMovie = await movieAPI.getMovie(this.props.match.params.id);
+    this.setState({ 
+      movie: apiMovie,
+      loading: false, 
+  });
+  }
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    if (this.state.loading === true ) {
+      return <Loading />
+    }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h2>{`Title: ${title}`}</h2>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${this.movieId}/edit`}>EDITAR</Link><br />
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
