a07cbbdde47aa64fb9ddb8c9b1d0d1ea157b00e9
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-26 18:14:45 -0300

    add MovieCard component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class MovieCard extends React.Component {
   render() {
+    const { title, storyline, id } = this.props.movie;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h3>{title}</h3>
+        <h4>{storyline}</h4>
+        <Link to={`/movies/${id}`} >VER DETALHES</Link>
       </div>
     );
   }
