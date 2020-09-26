abcfda1d1200fe5130f9b493ae919568e2a94a88
Igor Leão <igorleao.eng@gmail.com>
2020-09-26 19:44:03 -0300

    cc movieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
-
-
 class MovieDetails extends Component {
   constructor(props) {
     super(props);
