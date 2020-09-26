febeaec52f28cc7d5adb4c0a118b8617e34a62ab
DekoPossas <andrepossaster@gmail.com>
2020-09-26 20:04:01 -0300

    movie list enviando os filmes via API

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,14 +10,27 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      isLoading: true,
     }
   }
 
-  render() {
-    const { movies } = this.state;
+  async componentDidMount() {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      movies,
+      isLoading: false,
+    })
+  }
 
+  render() {
+    const { movies, isLoading } = this.state;
+   
     // Render Loading here if the request is still happening
 
+    if (isLoading === true) {
+      return (<Loading />)
+    }
+    
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
