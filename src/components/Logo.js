82f0620c91dba30a8cde3cf60a536bb34250b919
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-26 20:08:26 -0300

    iniciando as correcoes do code climate2

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class Logo extends Component {
   render() {
-    const {imagem} = this.props;
-    return(
+    const { imagem } = this.props;
+    return (
       <div className="cubo">
-        <img src={ imagem } alt="imagem" width="40" />
-        <img src={ imagem } alt="girando" width="40" />
-        <img src={ imagem } alt="com o logo" width="40" />
-        <img src={ imagem } alt="do coyote" width="40" />
+        <img src={imagem} alt="girando" width="40" />
+        <img src={imagem} alt="com o logo" width="40" />
+        <img src={imagem} alt="imagem" width="40" />
+        <img src={imagem} alt="do coyote" width="40" />
       </div>
     );
   }
