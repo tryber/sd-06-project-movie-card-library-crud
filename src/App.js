bce8e5bff140e12df18df78493cfeff6e628bfe6
vbergadev <vbergadev@gmail.com>
2020-09-26 13:15:31 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route component={NotFound} />
-      </Switch>
-  </BrowserRouter>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route component={NotFound} />
+    </BrowserRouter>
   );
 }
 
