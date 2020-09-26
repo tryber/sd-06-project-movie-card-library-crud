aae1a727f8b8fc06939f2a88f6b112a5c6421525
danieleperse <danieleperse3@gmail.com>
2020-09-26 17:55:08 -0300

    css add nas paginas: inicio, details, card e header.

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Loading from '../components/Loading';
 import { getMovie, deleteMovie } from '../services/movieAPI';
+import './movieDetails.css';
 
 class MovieDetails extends React.Component {
   constructor() {
@@ -38,16 +39,19 @@ class MovieDetails extends React.Component {
 
     return (
       <div data-testid="movie-details">
-        {this.state.isLoading ? <Loading /> : <div>
-          <img alt="Movie Cover" src={`../${imagePath}`} className="detailImg" />
-          <p>{`Title: ${title}`}</p>
+        {this.state.isLoading ? <Loading /> :
+        <div className="movie-detail">
+          <img alt="Movie Cover" src={`../${imagePath}`} />
+          <p><strong>{`Title: ${title}`}</strong></p>
           <p>{`Subtitle: ${subtitle}`}</p>
           <p>{`Storyline: ${storyline}`}</p>
           <p>{`Genre: ${genre}`}</p>
           <p>{`Rating: ${rating}`}</p>
-          <Link to={`/movies/${id}/edit`} className="linkDown">EDITAR</Link>
-          <Link to="/" className="linkDown">VOLTAR</Link>
-          <Link to="/" className="linkDown" onClick={this.deleteTheMovie}>DELETAR</Link>
+          <div className="link-detail">
+            <Link to={`/movies/${id}/edit`} className="linkDown">EDITAR</Link>
+            <Link to="/" className="linkDown">VOLTAR</Link>
+            <Link to="/" className="linkDown" onClick={this.deleteTheMovie}>DELETAR</Link>
+          </div>
         </div>}
       </div>
     );
