0c220387f28126ce26fb32f20fe4bf49c4b2fc4e
rodrigoderkian <derkianpromo@gmail.com>
2020-09-27 15:25:21 -0300

    Implementando EditMovie, que exibe mensagem de carregamento, apresenta formulario (MovieForm), e, ao clicar em submit, faz requisiçao na API, atualizando o filme, e redirecionando a pagina inicial

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import propTypes from 'prop-types';
+
 import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
