14e6e3c977a1f530c1bc0345ee90c2561bc24424
rodrigoderkian <derkianpromo@gmail.com>
2020-09-27 10:50:22 -0300

    Implementando MovieCard, que deve exibir titulo, sinopse, e um link direcionando para pagina de detalhes do filme

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 class MovieCard extends React.Component {
   render() {
+    const { movie: { id, title, storyline } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h4>{title}</h4>
+        <h5>{storyline}</h5>
+        <Link to={`/movie/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
