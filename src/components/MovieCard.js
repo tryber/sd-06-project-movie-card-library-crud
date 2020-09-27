3dfd13f5d816539d1dad2b01417376636ec6dfd5
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-27 18:21:29 -0300

    verificando o nome da class, movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,13 +6,13 @@ class MovieCard extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div className="movie-card">
+      <div className="movie-card" data-testid="movie-card">
         <img alt="Imagem do Filme" className="movie-card-image" src={movies.imagePath} />
         <div className="movie-card-boy">
           <h4 className="movie-card-title">{movies.title}</h4>
           <h5 className="movie-card-subtitle">{movies.subtitle}</h5>
           <p className="movie-card-storyline">{movies.storyline}</p>
-          <Link to={`movies/${movies.id}`}>Ver Detalhes</Link>
+          <Link to={`movies/${movies.id}`}>VER DETALHES</Link>
         </div>
       </div>
     );
