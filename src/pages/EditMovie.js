f478e92b05a4025e828f512477723556ac9231c9
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-27 15:15:17 -0300

    Implementada requisiçao de filme no componente EditMovie

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -6,11 +6,34 @@ import * as movieAPI from '../services/movieAPI';
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: {},
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchMovieForEdition();
+  }
+
+  async fetchMovieForEdition() {
+    this.setState({ status: 'loading'}, async () => {
+      const { id } = this.props.match.params;
+      const requestReturn = await movieAPI.getMovie(id);
+      this.setState({
+        status: 'ready',
+        movie: requestReturn,
+        shouldRedirect: true,
+      });
+    });
+  }
+
   handleSubmit(updatedMovie) {
+    // call API to update movie
+    // redirect to main page
+    // see shouldRedirect
   }
 
   render() {
@@ -22,7 +45,7 @@ class EditMovie extends Component {
     if (status === 'loading') {
       // render Loading
     }
-
+    console.log(this.props.match.params);
     return (
       <div data-testid="edit-movie">
         <MovieForm movie={movie} onSubmit={this.handleSubmit} />
