4accf01ab716a805619eb844862c049f290902d3
rodrigoderkian <derkianpromo@gmail.com>
2020-09-27 15:23:58 -0300

    Implementando EditMovie, que exibe mensagem de carregamento, apresenta formulario (MovieForm), e, ao clicar em submit, faz requisiçao na API, atualizando o filme, e redirecionando a pagina inicial

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import propTypes from 'prop-types';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchList = this.fetchList.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    this.fetchList();
+  }
+  
+  async fetchList() {
+    const movieId = this.props.match.params.id;
+    const idMovie = await movieAPI.getMovie(movieId);
+    this.setState({
+      movie: idMovie,
+      status: ""
+    });
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    })
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />
     }
 
     return (
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -43,6 +43,8 @@ class MovieDetails extends Component {
           <p>{`Rating: ${rating}`}</p>
           <div>
             <Link to="/">VOLTAR</Link>
+          </div>
+          <div>
             <Link to={`/movies/${id}/edit`}>EDITAR</Link>
           </div>
         </div>
