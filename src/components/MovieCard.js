7722e6aec521fc165402bf515f6ba770ea1260ec
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-26 23:21:38 -0300

    Implementada a requisiçao da lista de filmes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, imagePath, storyline } = movie;
+    console.log(movie);
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" src={imagePath} />
+        <div>
+          <h4>{title}</h4>
+          <h5>{storyline}</h5>
+        </div>
+        <div>
+          <Link>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -7,20 +8,49 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.showMovies = this.showMovies.bind(this);
+    this.fetchMovies = this.fetchMovies.bind(this);
+
     this.state = {
       movies: [],
+      isLoading: true,
     }
   }
 
-  render() {
+  async fetchMovies() {
+    this.setState({ isLoading: true }, async () => {
+      const requestReturn = await movieAPI.getMovies();
+      console.log(requestReturn);
+      this.setState({
+        movies: requestReturn,
+        isLoading: false,
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  showMovies() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
+    );
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    // Render Loading here if the request is still happening
+
+    return (
+      <div>
+        {isLoading ? <Loading /> : this.showMovies()}
+      </div>
+      
     );
   }
 }
