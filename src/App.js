3c1145838cdf855feff7bc4e8a89c34f58d66aed
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-09-27 16:00:35 -0300

    req01 cc03

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       Movie Card Library CRUD
       <BrowserRouter>
         <Switch>
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
           <Route exact path="/" component={MovieList} />
           <Route exact path="*" component={NotFound} />
         </Switch>
