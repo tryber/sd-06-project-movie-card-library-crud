b39ed13496aa687da95404eb89bfc82b4dfadf38
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-27 01:37:55 -0300

    Gera cartões de filme com link para maiores detalhes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom' 
 
 class MovieCard extends React.Component {
   render() {
+    const {movie} = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <div>
+          <img src={movie.imagePath} alt={`imagem do filme ${movie.title}`} />
+          <h2>{movie.title}</h2>
+          <h3>{movie.subtitle}</h3>
+          <p>{movie.storyline}</p>
+        </div>
+        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
       </div>
     );
   }
