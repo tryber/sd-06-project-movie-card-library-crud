d52f9ef82efc7246eaf7377e8d6cb40111026966
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-27 19:38:19 -0300

    corrigindo informacoes dos filmes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,11 +7,11 @@ class MovieCard extends React.Component {
     const { movies } = this.props;
     return (
       <div className="movie-card" data-testid="movie-card">
-        <img alt="Imagem do Filme" className="movie-card-image" src={movies.imagePath} />
+        <img className="movie-card-image" src={movies.imagePath} alt="Imagem do Filme" />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{movies.title}</h4>
-          {/* <h5 className="movie-card-subtitle">{movies.subtitle}</h5>
-          <p className="movie-card-storyline">{movies.storyline}</p> */}
+          {/* <h5 className="movie-card-subtitle">{movies.subtitle}</h5> */}
+          <p className="movie-card-storyline">{movies.storyline}</p>
           <Link to={`movies/${movies.id}`}>VER DETALHES</Link>
         </div>
       </div>
