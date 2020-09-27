b8de690e3b8e3e393fde6150fcd78cd0b23e92a2
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-09-26 21:33:02 -0300

    adicionando componentes Loading e Moviecard, renderização em App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
 
-function App() {
-  return (
-    <div>M</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Movie card Library CRUD</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={MovieList} />
+            <Route path="/movies/:id/edit" component={EditMovie} />
+            <Route path="/movies/new" component={NewMovie} />
+            <Route path="/movies/:id" component={MovieDetails} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types'
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={movie.imagePath} alt={movie.title} />
+        <div>
+          <h1>{movie.title}</h1>
+          <h2>{movie.subtitle}</h2>
+          <p>{movie.storyline}</p>
+        </div>
+        <div>
+          <Link to={`/movies/${movie.id}`}>
+            VER DETALHES
+          </Link>
+        </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: propTypes.shape({
+    imagePath: propTypes.string.isRequired,
+    title: propTypes.string.isRequired,
+    storyline: propTypes.string.isRequired,
+    storyline: propTypes.string.isRequired,
+  }).isRequired
+};
 
 export default MovieCard;
