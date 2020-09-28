0b41007b2ceff3d93db89ebfebd7753c8a8b551a
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-28 13:52:02 -0300

    Update MovieDetails.js

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
@@ -55,8 +55,12 @@ class MovieDetails extends Component {
   }
 }
 
-MovieDetails.PropTypes = {
-  match: PropTypes
-}
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
 
 export default MovieDetails;
