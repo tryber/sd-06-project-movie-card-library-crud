7dcf9b1859d619309ae8beb9647289ee327a3a88
vbergadev <vbergadev@gmail.com>
2020-09-28 11:52:01 -0300

    requisito 6

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
-
+import Header from './components/Header';
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
