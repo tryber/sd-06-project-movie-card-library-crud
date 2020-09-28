5b9cd3be0edae5d60d25d63e45325a9933a2f18f
ilanribeiro <ilanribeiro@gmail.com>
2020-09-28 14:45:33 -0300

    requirement 6 completed - NewMovie

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
@@ -6,16 +7,32 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      movie: {},
+      status: '',
+    }
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(newMovie) {
+
+  async handleSubmit(newMovie) {
+    await movieAPI.createMovie(newMovie);
+    this.setState({ status: 'redirect' });
   }
 
   render() {
+    const { status, movie } = this.state;
+
+    if (status === 'redirect') {
+      return <Redirect to="/" />;
+    }
+
+
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={this.handleSubmit} />
+        <MovieForm movie={movie} onSubmit={this.handleSubmit} />
       </div>
     );
   }
