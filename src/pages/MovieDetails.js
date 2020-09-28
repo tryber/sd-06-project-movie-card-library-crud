e1e9b4513aa301ea4aa1ea9601ee305c536f1e8e
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-28 11:57:22 -0300

    Update MovieDetails.js

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -23,8 +23,8 @@ class MovieDetails extends Component {
     this.setState({
       loading: true,
     }, async () => {
-      const { match } = this.props;
-      const requestMovie = await movieAPI.getMovie(match.params.id);
+      const { id } = this.props.match.params;
+      const requestMovie = await movieAPI.getMovie(id);
 
       this.setState({
         details: requestMovie,
@@ -38,18 +38,20 @@ class MovieDetails extends Component {
     const { loading } = this.state;
 
     return (
-      <div data-testid="movie-details">
-        { loading ? <Loading /> : <div>
-          <img alt="Movie Cover" src={`../${imagePath}`} />
-          <h1>{`Título: ${title}`}</h1>
-          <p>{`Subtítulo: ${subtitle}`}</p>
-          <p>{`Sinopse: ${storyline}`}</p>
-          <p>{`Gênero: ${genre}`}</p>
-          <p>{`Avaliação: ${rating}`}</p>
-
-          <Link to={`/movies/${id}/edit`}>EDITAR</Link>
-          <Link to="/">VOLTAR</Link>
-        </div>
+      <div>
+        { loading ? <Loading /> :
+          <div data-testid="movie-details">
+            <img alt="Movie Cover" src={`../${imagePath}`} />
+            <p>{`Title: ${title}`}</p>
+            <p>{`Subtitle: ${subtitle}`}</p>
+            <p>{`Storyline: ${storyline}`}</p>
+            <p>{`Genre: ${genre}`}</p>
+            <p>{`Rating: ${rating}`}</p>
+            <div>
+              <div><Link to={`${id}/edit`}>EDITAR</Link></div>
+              <div><Link to="/">VOLTAR</Link></div>
+            </div>
+          </div>
         }
       </div>
     );
