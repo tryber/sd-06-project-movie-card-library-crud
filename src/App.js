d558c0a0b01dd4f52db64b96c10a735163178c37
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-28 19:06:43 -0300

    requisito 01 - cc 1.4 - add route 1.0.3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
 import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 import movies from './services/movieData';
 
 
@@ -13,7 +15,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/movie/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path=" " component={ Notfound }/>
         <Link />
         <div className="app">
           <div>Movie Card Library CRUD</div>
