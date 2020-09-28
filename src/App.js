c4e6b9c2148dd9fd19422e0dda75c1e5fd4bb6b9
Isabela Rosa <isabelarosa954@gmail.com>
2020-09-28 11:52:00 -0300

    alterações iniciais em app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>Movie Card Library CRUD
+      <BrowserRouter>
+        <Route path="/movies" component={MovieList} />
+      </BrowserRouter>
+    </div>
   );
 }
 
