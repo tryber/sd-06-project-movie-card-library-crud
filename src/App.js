15af1170612f71db573ac64bf6c532ea965e5d0f
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-28 18:32:23 -0300

    requisito 01 - cc 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import { 
+import {
   Route,
   Link,
-  BrowserRouter as Router } from 'react-router-dom'
+  BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import MovieList from './pages/MovieList'
-import movies from './services/movieData'
+import MovieList from './pages/MovieList';
+import movies from './services/movieData';
 
 
 class App extends Component {
   render() {
-  return ( 
-    <Router>
-     <div className="app">
-     
-       <div>Movie Card Library CRUD</div>
-       
-     
-    </div>
-    </Router>
-  )
-}
+    return (
+      <Router>
+      <Route></Route>
+      <Link></Link>
+        <div className="app">
+          <div>Movie Card Library CRUD</div>
+          <MovieList movies={movies} />
+        </div>
+      </Router>
+    );
+  }
 }
 export default App;
