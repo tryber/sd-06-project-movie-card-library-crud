99264b6f1be8f62d3b3145d5ecc6d9a1774283a5
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-29 20:05:06 -0300

    corrigindo code climate2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
+import Header from './components/Header';
 
 function App() {
   return (
