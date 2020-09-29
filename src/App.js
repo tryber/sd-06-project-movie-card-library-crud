fd66b0817f39212c4044eb70d4ba957dc6882e32
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-09-28 22:37:19 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import MovieList from './pages/MovieList';
@@ -12,10 +12,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={MovieList} />
-        <Route path='/movies/:id' component={MovieDetails} />
-        <Route path='/movies/new' component={NewMovie} />
-        <Route path='/movies/:id/edit' component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
