a306df908c159bce1fab5aeadaddb38e3c92f73e
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-29 00:37:30 -0300

    Implementada funcionalidade de adiçao de novo filme

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,13 +3,15 @@ import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
+import { Link } from 'react-router-dom';
 
 class MovieList extends Component {
   constructor() {
     super();
 
-    this.showMovies = this.showMovies.bind(this);
+    this.renderMovies = this.renderMovies.bind(this);
     this.fetchMovies = this.fetchMovies.bind(this);
+    this.renderNewMovieLink = this.renderNewMovieLink.bind(this);
 
     this.state = {
       movies: [],
@@ -31,7 +33,7 @@ class MovieList extends Component {
     });
   }
 
-  showMovies() {
+  renderMovies  () {
     const { movies } = this.state;
     return (
       <div data-testid="movie-list">
@@ -40,14 +42,24 @@ class MovieList extends Component {
     );
   }
 
+  renderNewMovieLink () {
+    return (<Link to="/movies/new">ADICIONAR CARTÃO</Link>);
+  }
+
+
   render() {
     const { isLoading } = this.state;
+
+    if (isLoading) {
+      return (<Loading />);
+    }
 
     // Render Loading here if the request is still happening
 
     return (
       <div>
-        {isLoading ? <Loading /> : this.showMovies()}
+        {this.renderMovies()}
+        {this.renderNewMovieLink()}
       </div>
     );
   }
