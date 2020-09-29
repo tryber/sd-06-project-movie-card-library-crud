a379c12afea35bc0fa4af4916961081186a36a61
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-09-28 23:27:10 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-        <Route exact path="/" component={MovieList} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/movies/new" component={NewMovie} />
-        <Route exact path="/movies/:id/edit" component={EditMovie} />
-        <Route component={NotFound} />
+      <Route exact path="/" component={MovieList} />
+      <Route exact path="/movies/:id" component={MovieDetails} />
+      <Route exact path="/movies/new" component={NewMovie} />
+      <Route exact path="/movies/:id/edit" component={EditMovie} />
+      <Route component={NotFound} />
     </BrowserRouter>
   );
 }
