d915fcf12cde6a8abdd6f15147a8e8b2d41be425
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-29 20:09:51 -0300

    corrigindo code climate3

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/movies/:id/edit" component={EditMovie} />
-          <Route exact path="/movies/new" component={NewMovie} />
-          <Route exact path="/movies/:id" component={MovieDetails} />
-          <Route exact path="/" component={MovieList} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
