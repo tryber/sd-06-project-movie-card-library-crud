ef350b4e0b89f56e3d55cf0afc28cc4e6ce89c6f
Danilo <dansantosalfa@gmail.com>
2020-10-01 18:40:45 -0300

    Finalizando EditMovie

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: undefined,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  async componentDidMount() {
+    const id = this.props.match.params.id;
+    const fetchData = await movieAPI.getMovie(id);
+    this.setState({ movie: fetchData });
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
-
-    if (status === 'loading') {
-      // render Loading
-    }
-
+    const { shouldRedirect, movie } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
+    if (!movie) return <Loading />;
     return (
       <div data-testid="edit-movie">
         <MovieForm movie={movie} onSubmit={this.handleSubmit} />
