8e6f44bdd581aa135f1e5e4463609c2fa24b8e35
Pedro <pedroedu77@gmail.com>
2020-10-01 02:38:30 -0300

    Requisito 3 Revisado e Concluido cc

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -15,9 +15,9 @@ class MovieCard extends React.Component {
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-      </div>
-      <Link to={`/movies/${id}`}>VER DETALHES</Link>
-      <Rating rating={rating} />
+        </div>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
+        <Rating rating={rating} />
       </div>
     );
   }
