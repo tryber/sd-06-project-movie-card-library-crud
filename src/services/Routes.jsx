ea20eefdac02f2ec9d369c904620508261f6d4e3
Danilo <dansantosalfa@gmail.com>
2020-10-01 15:57:55 -0300

    Criando Serviço Routes para rederizar todas as rotas, emplementando no App.js e mudando o Loading.js

---
+++ src/services/Routes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import MovieList from '../pages/MovieList';
+import MovieDetails from '../pages/MovieDetails';
+import NewMovie from '../pages/NewMovie';
+import EditMovie from '../pages/EditMovie';
+import NotFound from '../pages/NotFound';
+
+
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    );
+  }
+}
+export default Routes;
