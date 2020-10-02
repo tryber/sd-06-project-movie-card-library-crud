deb0f530be6825928cc18e67604b21afd41b1843
Pedro Risso <pmerchiorattor@gmail.com>
2020-10-02 10:19:24 -0300

    feat: after a new movie added redirect to /

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = { redirect: false }
   }
-  
-  handleSubmit(newMovie) {
+
+  async handleSubmit(newMovie) {
+    const resp = await movieAPI.createMovie(newMovie);
+    this.setState({ redirect: true });
   }
-  
+
   render() {
+    const { redirect } = this.state;
     return (
-      <div data-testid="new-movie">
-        <MovieForm onSubmit={this.handleSubmit} />
-      </div>
+      redirect
+        ? <Redirect to="/" />
+        : <div data-testid="new-movie">
+          <MovieForm onSubmit={this.handleSubmit} />
+        </div>
     );
   }
 }
