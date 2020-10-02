7135cca78810904b8ea3983ec7f8eb1087839583
alcantara2509 <alcantara2509@gmail.com>
2020-10-02 10:05:55 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <div>
+      Movie Card Library CRUD
+      <Router>
+        <Switch>
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/" component={MovieList} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
