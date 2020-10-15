804e03e943fef96ceb6f1eae2f29ecb88a559b31
Rick Sousa <rickssti@outlook.com>
2020-10-15 19:47:49 -0300

    :bug: CodeClimate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails'
+import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
@@ -13,7 +13,7 @@ function App() {
       <switch>
         <Route path="/movies/:id" component={MovieDetails} />
         <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route path="/movies/new" component={NewMovie}  />
+        <Route path="/movies/new" component={NewMovie} />
         <Route path="/" component={MovieList} />
         <Route component={NotFound} />
       </switch>
