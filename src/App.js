56179a99a0117815eea999463f7a1094c2051175
Stefano <stefano.branz@gmail.com>
2020-10-15 18:57:15 -0300

    Requirement 1 push 02 - code climate fixes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route exact path="/" component={ MovieList } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="" component={ NotFound } />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="" component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
