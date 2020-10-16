6e377be410942e4acf365b54b830e321c78499b1
Rick Sousa <rickssti@outlook.com>
2020-10-16 16:46:53 -0300

    :bug: CodeClimate

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+
 
 class EditMovie extends Component {
   constructor(props) {
@@ -29,19 +31,27 @@ class EditMovie extends Component {
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      return <Redirect to="/" />
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      return <Loading />
+      return <Loading />;
     }
 
     return (
       <div data-testid="edit-movie">
-        <MovieForm movie={movie} onSubmit={this.handleSubmit} />
+        <MovieForm movie={movie} onSubmit={this.handleSubmit} />;
       </div>
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default EditMovie;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import { Link } from 'react-router-dom';
+
 
 class MovieDetails extends Component {
-  constructor(props) {
+  constructor() {
     super();
 
     this.state = {
@@ -22,7 +23,7 @@ class MovieDetails extends Component {
 
   render() {
     const { movie, loading } = this.state;
-    if (loading === true) { return <Loading /> }
+    if (loading === true) { return <Loading /> };
     const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
@@ -33,11 +34,19 @@ class MovieDetails extends Component {
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{rating}</p>
-        <Link to={`/`}>VOLTAR </Link>
+        <Link to='/'>VOLTAR </Link>
         <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
