108a915a207d7793ed5403aea2275bc7e30f1604
lucasvr39 <lucasveras1994@gmail.com>
2020-10-16 17:52:23 -0300

    add reqs 1, 2 and 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div data-testid="movie-card" className="movie-card">
+       <img alt="Movie's Cover" src={imagePath} />
+       <div>
+        <h4 data-testeid="movie-card-title">{title}</h4>
+        <h5 data-testeid="movie-card-subtitle">{subtitle}</h5>
+        <p>{storyline}</p>
+        <p>{rating}</p>   
+       </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = { movie: PropTypes.shape.isRequired };
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading'
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +10,37 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
-    }
+      load: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getMoviesList();
+  }
+
+  async getMoviesList() {
+    this.setState(
+      // while loading content
+     { loading: true },
+      async () => {
+        // calls movieAPI to get all movies, create and object and set in page's state 
+        const movies = await movieAPI.getMovies();
+        this.setState({
+          load: false, // after all content is properly loaded in the page
+          movies,
+        });
+      }
+    );
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, load } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {load ? <Loading /> : movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
