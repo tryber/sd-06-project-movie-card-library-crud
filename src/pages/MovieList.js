7ff8cd863fb8af6f66876c909f362a24613da06e
Rick Sousa <rickssti@outlook.com>
2020-10-16 17:12:19 -0300

    :sparkles: New Movie implemented

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,6 +3,7 @@ import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
+import { Link } from 'react-router-dom';
 
 class MovieList extends Component {
   constructor() {
@@ -25,6 +26,7 @@ class MovieList extends Component {
 
     return (
       <section>
+        <Link to="movies/new">ADICIONAR CARTÃO</Link>
         <div data-testid="movie-list">
           {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
         </div>
