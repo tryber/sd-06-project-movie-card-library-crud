ce1556949e409ee4e98495de64c7a52580a474e5
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-10-17 03:27:32 -0300

    13 passed

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,21 +2,51 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    return this.fetchAPIMovie();
+  }
+
+  async fetchAPIMovie() {
+    const apiMovie = await movieAPI.getMovie(this.props.match.params.id);
+    this.setState({ 
+      movie: apiMovie,
+      loading: false,
+    });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (this.state.loading === true) {
+      return <Loading />
+    }
+
+
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
+        <h4>{title}</h4>
         <img alt="Movie Cover" src={`../${imagePath}`} />
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${this.props.match.params.id}/edit`}>EDITAR</Link><br />
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
