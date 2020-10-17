3ac5cf4a8e58d6ae72c8ec541505276b7472b76c
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-10-17 15:59:10 -0300

    18 passed

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -32,6 +33,7 @@ class MovieList extends Component {
 
     return (
       <div data-testid="movie-list">
+        <Link to={"/movies/new"}>ADICIONAR CARTÃO</Link>
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      redirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie);
+    this.setState({
+      redirect: true,
+    });
   }
 
   render() {
+
+    if (this.state.redirect === true) {
+      return (
+        <Redirect to="/" />
+      );
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
