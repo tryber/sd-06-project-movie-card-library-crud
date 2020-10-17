a1129ded78081c913b7f5954b7d57f35b453c633
Aloizio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-10-17 15:26:55 -0300

    16 passed

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { Loading } from '../components';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      loading: true,
+      id: this.props.match.params.id,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchAPIMovie();
+  }
+
+  async fetchAPIMovie() {
+    const apiMovie = await movieAPI.getMovie(this.state.id);
+    this.setState({
+      movie: apiMovie,
+      loading: false
+    });
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { loading, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (loading === true) {
+      return <Loading />;
     }
 
     return (
@@ -30,5 +53,7 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = { match: PropTypes.objectOf(Array).isRequired };
 
 export default EditMovie;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -27,9 +27,6 @@ class MovieDetails extends Component {
   }
 
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
     if (this.state.loading === true) {
       return <Loading />;
     }
