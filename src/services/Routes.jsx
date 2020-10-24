5f1b1acbdee5259c9e92a6af26f7925d374c9ebf
Flavio Oliveira <flavsoliver@gmail.com>
2020-10-24 19:32:31 -0300

    vamos lá...

--- src/services/Routes.jsx
+++ src/services/Routes.jsx
@@ -6,10 +6,10 @@ export default class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
         <Route exact path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
         <Route component={NotFound} />
       </Switch>
     );
