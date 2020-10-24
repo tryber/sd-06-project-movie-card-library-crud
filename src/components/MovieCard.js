92acc90cd2a4d59d2d492d0af931a70e35142d17
Flavio Oliveira <flavsoliver@gmail.com>
2020-10-24 17:31:45 -0300

    enviando arquivo de rotas - requisito 01

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { id, title, storyline } } = this.props;
+
     return (
       <div data-testid="movie-card">
         Movie Card
+        <div>
+          <h1>{title}</h1>
+          <p>{storyline}</p>
+        </div>
       </div>
     );
   }
