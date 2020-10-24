92372c11156957b485528840b8231310b7d1fb72
vapatty6848 <vapatty@gmail.com>
2020-10-24 18:03:53 -0300

    codeclimate

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -5,11 +5,11 @@ import Loading from '../components/Loading';
 import PropTypes from 'prop-types';
 class EditMovie extends Component {
   constructor(props) {
-  super(props);
-    this.state = {
-      movie: [],
-      isloading: true,
-    };
+    super(props);
+      this.state = {
+        movie: [],
+        isloading: true,
+      };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
@@ -27,7 +27,7 @@ class EditMovie extends Component {
       movie: filme,
       isLoading: false,
     });// atualiza o estado do filme
-}
+  }
   render() {
     const { shouldRedirect, movie, isLoading } = this.state;
     if (shouldRedirect) {
