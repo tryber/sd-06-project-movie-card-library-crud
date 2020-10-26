3ddc51d36e35bbaeb25bd9bca13aa8949620108c
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-10-26 12:00:32 -0300

    add ADICIONAR CARTÃO link - cc01

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -38,7 +38,7 @@ class MovieList extends Component {
           {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
         </div>
         <div>
-          <Link to={`/movies/new`}>ADICIONAR CARTÃO</Link>
+          <Link to={'/movies/new'}>ADICIONAR CARTÃO</Link>
         </div>
       </div>
     );
