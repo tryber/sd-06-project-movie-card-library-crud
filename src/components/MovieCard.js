8927b5858c70ed9eade44cb40c7fbf025ddc1f04
Cass007 <thecastieel07@gmail.com>
2020-10-30 04:02:50 -0300

    bora fio 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -18,5 +19,18 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.defaultProps = {
+  movie: '',
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string,
+  }),
+};
 
 export default MovieCard;
