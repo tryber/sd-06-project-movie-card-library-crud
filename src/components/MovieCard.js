a312343ccc1df6ca4cff37f5e7043c0be9114b3b
Flavio Oliveira <flavsoliver@gmail.com>
2020-10-31 16:00:34 -0300

    moviecard e moviedatails implementações req 4

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { id, title, storyline } } = this.props;
+    const { movie: { id, title, storyline, imagePath } } = this.props;
 
     return (
       <div data-testid="movie-card">
-        Movie Card
         <div>
           <h1>{title}</h1>
           <p>{storyline}</p>
           <h3>{id}</h3>
+          <img alt="Movie Cover" src={imagePath} />
         </div>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
@@ -25,5 +27,6 @@ MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
   }).isRequired,
 };
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+    };
+  }
+
+  
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
     const { title, storyline, imagePath, genre, rating, subtitle } = {};
+
+    // if (movies.length === 0) return <Loading />;
 
     return (
       <div data-testid="movie-details">
