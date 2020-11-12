058d03c1ece7b2548103acddd3d4053262f515ec
Marthus Sera <marthus2001@gmail.com>
2020-11-12 03:04:38 -0300

    Requirement 8

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
@@ -10,11 +10,13 @@ class MovieDetails extends Component {
     super();
 
     this.state = {
+      done: false,
       isFetching: true,
       movie: {},
     };
 
     this.fetchAPI = this.fetchAPI.bind(this);
+    this.deleteMovie = this.deleteMovie.bind(this);
   }
 
   componentDidMount() {
@@ -29,11 +31,20 @@ class MovieDetails extends Component {
     });
   }
 
+  async deleteMovie() {
+    await movieAPI.deleteMovie(this.props.match.params.id);
+    this.setState({
+      done: true,
+    });
+  }
+
   render() {
-    const { isFetching } = this.state;
+    const { isFetching, done } = this.state;
     const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     if (isFetching) return <Loading />;
+
+    if (done) return <Redirect to="/" />;
 
     return (
       <div data-testid="movie-details">
@@ -43,7 +54,8 @@ class MovieDetails extends Component {
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
-        <Link to={`/movies/${this.props.match.params.id}/edit`}>EDITAR</Link> <br />
+        <button onClick={this.deleteMovie}>DELETAR</button><br />
+        <Link to={`/movies/${this.props.match.params.id}/edit`}>EDITAR</Link><br />
         <Link to="/" >VOLTAR</Link>
       </div>
     );
