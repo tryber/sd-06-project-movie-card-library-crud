58b048de90b4df9561d9920cdad761d58839f53a
lucasvr39 <lucasveras1994@gmail.com>
2020-11-14 13:43:28 -0300

    fix req 1 by fix and implement req 6

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
@@ -8,6 +7,10 @@ class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      shouldRedirect: false
+    };
   }
 
   async handleSubmit(newMovie) {
